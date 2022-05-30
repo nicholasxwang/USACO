21ca098bf25c283d2f1b6fcbdce8782c336756bb
NicholasWang <nicholas.x.wang@gmail.com>
2022-05-30 14:07:03 -0700

    added lineup's testcases and improved efficiency

--- src/2011/Silver/Question2.java
+++ src/2011/Silver/Question2.java
@@ -35,14 +35,10 @@ class Question2{
             cows[i][1] = Integer.parseInt(temporary_unparsed[1]);
         }
         Arrays.sort(cows, Comparator.comparingDouble(o -> o[0]));
-        HashMap<Integer, ArrayList<Integer>> things = new HashMap<>();
+        HashSet<Integer> things = new HashSet<>();
         for (int i = 0; i<N; i++){
-            if (!things.containsKey(cows[i][1])){
-                things.put(cows[i][1], new ArrayList<Integer>());
-            }
-            things.get(cows[i][1]).add(cows[i][0]);
+            things.add(cows[i][1]);
         }
-        //System.out.println(things);
         int[] cow_1d = new int[N];
         for (int i = 0; i<N; i++){
             cow_1d[i] = cows[i][1];
@@ -51,26 +47,17 @@ class Question2{
         ArrayList<Integer> ans = new ArrayList<>();
         for (int i = 0; i<N; i++){
             for (int j = i; j<N; j++) {
-                if (cows[i][0] == 22 && cows[j][0] == 26){
-                    //System.out.println("We're Here");
-                }
-//                if ((j-i) > / <= / < things.keySet().size()){
-//                    System.out.println("FALSE-1 ("+cows[i][0]+", "+cows[j][0]+") \n j-i = "+(j-i)+" the size is "+things.keySet().size());
-//                    continue;
-//                }
                 int[] subset = Arrays.copyOfRange(cow_1d, i, j+1);
                 HashSet<Integer> subset_set = convert(subset);
                 boolean valid = true;
-                for (int k : things.keySet()){
+                for (int k : things){
                     if (!subset_set.contains(k)){
-                        //System.out.println("FALSE-2 ("+cows[i][0]+", "+cows[j][0]+") | Can't find "+k+" in "+subset_set);
                         valid = false;
                         break;
                     }
                 }
                 if (valid){
                     ans.add(cows[j][0]-cows[i][0]);
-                    //System.out.println("("+cows[i][0]+", "+cows[j][0]+")");
 
                 }
 
