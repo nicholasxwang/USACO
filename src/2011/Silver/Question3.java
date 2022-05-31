1e23ce85fca829d65180c2b782972b80368b3d3e
NicholasWang <nicholas.x.wang@gmail.com>
2022-05-31 15:12:38 -0700

    fixed some bugs

--- src/2011/Silver/Question3.java
+++ src/2011/Silver/Question3.java
@@ -1,25 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 class Question3{
-    public static void find_sum_of_squares(int number, int count){
-        int sqrt = (int)Math.sqrt(number);
-        int count = 0;
-        ArrayList<Integer> squares =  new ArrayList<>();
-        for (int i = 0; i<=sqrt; i++){
-            count += i*i;
-            squares.add(count);
+    public static boolean prime(int N){
+        for (int i = 2; i <=   N / 2; ++i) {
+            // condition for nonprime number
+            if (N % i == 0) {
+                return false;
+            }
         }
+        return true;
     }
-    public static void find_dimensions(int N){
+    public static void find_sum_of_squares(int number, int count){
+        if (count == 1 || prime(number)){
+            return;
+        }
+        System.out.println(count);
+        ArrayList<Integer> dimensions = find_dimensions(number);
+        //dimensions = dimensions.subList(0, dimensions.size()/2+1);
+        ArrayList<Integer> newdimensions = new ArrayList<Integer>();
+        for (int i = 0; i<dimensions.size()/2+1; i++){
+                    newdimensions.add(dimensions.get(i));
+                }
+
+        for (int dimension : newdimensions){
+            int smaller = dimension;
+            int larger = number/dimension;
+            larger = larger-smaller;
+            find_sum_of_squares(smaller*larger, count-1);
+        }
+
+    }
+    public static ArrayList<Integer> find_dimensions(int N){
         ArrayList<Integer> nums = new ArrayList<>();
         for (int i = 1; i<=N; i++){
             if (N % i == 0){
-                    
+                    nums.add(i);
             }
         }
+        return nums;
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] start = br.readLine().split(" ", 2);
+        int N = Integer.parseInt(start[0]);
+        int M = Integer.parseInt(start[1]);
+        int[] old = new int[M];
+        for (int i = 0; i<N; i++){
+            String temporary = br.readLine();
+            old[i] = Integer.parseInt(temporary);
+        }
         int number = 6;
-        System.out.println(find_sum_of_squares(number));
+        find_sum_of_squares(number, 3);
     }
 }
