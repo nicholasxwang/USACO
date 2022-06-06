0722497e51c766cde3d833741432919db328feb2
NicholasWang <nicholas.x.wang@gmail.com>
2022-06-06 08:03:18 -0700

    re-organized files

--- src/Silver/2011/December/Question2.java
+++ src/Silver/2011/December/Question2.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class doc {
+public class Question2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String beg = new br.readLine();
