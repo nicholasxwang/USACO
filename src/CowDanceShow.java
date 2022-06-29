4822b3c325319f6658dbb6a90c7175cca33a14a4
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-06-28 22:39:19 -0700

    got 2 cases right for reduction, debugged more CowDanceShow.java

--- src/CowDanceShow.java
+++ src/CowDanceShow.java
@@ -7,31 +7,34 @@ public class CowDanceShow {
             return -1;
         }
         ArrayList<Integer> cur = new ArrayList<>(K);
-=        int index = K-1;
+        int index = K-1;
         for (int i = 0; i<K; i++){
             cur.add(cows.get(i));
         }
         int timing = 0;
-        while (index != K){
+        while (index != cows.size()){
 //            timing++;
-            System.out.println("The Cows: "+cur);
-            int to_be_removed = -1;
+//            System.out.println("("+index+") The Cows: "+cur);
+            ArrayList<Integer> to_be_removed = new ArrayList<>();
             for (int i = 0; i<cur.size(); i++){
 //                cur.set(i,cur.get(i)-1);
                 if (cur.get(i) == 0){
                     index+=1;
                     //to_be_removed = index;
-                    to_be_removed = i;
+                    to_be_removed.add(i);
                 }else{
                     cur.set(i,cur.get(i)-1);
                 }
 
 
             }
-            if (to_be_removed != -1){
-                cur.remove(to_be_removed);
-                if (index < cows.size())
-                    cur.add(cows.get(index));
+            if (to_be_removed.size() > 0){
+                for (int el = 0; el<to_be_removed.size(); el++){
+                    cur.remove(to_be_removed.get(el));
+                    if (index < cows.size())
+                        cur.add(cows.get(index));
+                }
+
             }
             timing++;
         }
@@ -49,8 +52,17 @@ public class CowDanceShow {
             //a.set(i, Integer.parseInt(b.readLine()));
             a.add(Integer.parseInt(b.readLine()));
         }
-        for (int i = 1; i<T; i++){
+        for (int i = 1; i<a.size(); i++){
             System.out.println("["+i+"]  "+calculate(a, i));
+        }
+
+        int l_pointer= 0;
+        int r_pointer = a.size();
+        while (true){
+
+            int value = calculate(a, l_pointer);
+            System.out.println("["+l_pointer+"]  "+value);
+
         }
 
     }
--- src/FieldReduction.java
+++ src/FieldReduction.java
@@ -1,6 +1,8 @@
 import java.io.*;
+import java.util.*;
 
 public class FieldReduction {
+
     public static int distance(int x1, int y1, int x2, int y2){
         return ((x1-x2)*(x1-x2) +  (y1-y2)*(y1-y2));
     }
@@ -8,13 +10,57 @@ public class FieldReduction {
         BufferedReader b = new BufferedReader(new FileReader("./reduce.in"));
         int N = Integer.parseInt(b.readLine());
         int[][] a = new int[N][2];
+        float average_x = 0;
+        float average_y = 0;
         for (int i = 0; i<N; i++){
             String[] t = b.readLine().split(" ");
             a[i][0] = Integer.parseInt(t[0]);
+            average_x += a[i][0];
             a[i][1] = Integer.parseInt(t[1]);
+            average_y +=  a[i][1];
         }
-        // find a cluster in them middle\=
-        //Task: Determine the most NEAR cows
+        average_x = average_x / N;
+        average_y = average_y / N;
+        Hashtable<Integer, Integer> dist = new Hashtable<>();
+        for (int i = 0; i<N; i++){
+            dist.put(i, distance(a[i][0], a[i][1], (int) average_x, (int) average_y));
+        }
+
+        //Transfer as List and sort it
+        ArrayList<Map.Entry<?, Integer>> dist2 = new ArrayList(dist.entrySet());
+        Collections.sort(dist2, new Comparator<Map.Entry<?, Integer>>(){
+
+            public int compare(Map.Entry<?, Integer> o1, Map.Entry<?, Integer> o2) {
+                return o1.getValue().compareTo(o2.getValue());
+            }});
+        int s_x = 999999999;
+        int b_x = -1;
+        int s_y = 999999999;
+        int b_y = -1;
+
+        for (int s = 0; s<dist2.size()-3; s++){
+            int key = (int) dist2.get(s).getKey();
+            int[] data = a[key];
+            if (data[0] < s_x){
+                s_x = data[0];
+            }
+            if (data[1] < s_y){
+                s_y = data[1];
+            }
+            if (data[0] > b_x){
+                b_x = data[0];
+            }
+            if (data[1] > b_y){
+                b_y = data[1];
+            }
+        }
+        //System.out.println((s_x - b_x)*(s_y - b_y));
+        PrintWriter printWriter = new PrintWriter ("reduce.out");
+        printWriter.println((s_x - b_x)*(s_y - b_y));
+        printWriter.close ();
+
+
+
 
     }
 
