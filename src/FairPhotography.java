b52226f270059d5ab0f7a8e2a0b0be8a49d38c83
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-07-11 21:21:41 -0700

    stuf

--- src/FairPhotography.java
+++ src/FairPhotography.java
@@ -1,90 +1,61 @@
 import java.io.*;
 import java.util.*;
-class Cow implements Comparable<Cow>{
-    int index;
-    char type;
-    public Cow(int index_, char type_){
-        this.index = index_;
-        this.type = type_;
-
-    }
-    @Override
-    public int compareTo(Cow c)
-    {
-        return this.index - c.index;
-    }
-
-}
 public class FairPhotography {
     public static void main(String[] args) throws IOException{
         //Initialized Variables
         String s;
         String[] parsed;
-        Cow cow;
-        int N;
-        int i;
-        Cow[] c;
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         BufferedReader br = new BufferedReader(new FileReader("fairphoto.in"));
 
 
 
         //Read Input
-        N = Integer.parseInt(br.readLine());
-        c = new Cow[N];
-        for (i = 0; i<N; i++){
+        int N = Integer.parseInt(br.readLine());
+
+        Hashtable<Integer, String> h = new Hashtable<>();
+        int[] a = new int[N];
+        for (int i = 0; i<N; i++){
             s = br.readLine();
             parsed = s.split(" ");
-            cow = new Cow(Integer.parseInt(parsed[0]), parsed[1].toCharArray()[0]);
-            c[i] = cow;
+            a[i] = Integer.parseInt(parsed[0]);
+            h.put(Integer.parseInt(parsed[0]), parsed[1]);
         }
-
-
-
-        //Sort
-        Arrays.sort(c);
-
-
-
-        //Apply 2 pointers algo
-        //If middle is spotted: DEAD
-        //If middle is white, change to spotted
-
-        int l = 0;
-        int r = 0;
-        char prev_cow = '-';
-        char last_cow = '-';
-        int MAX = 0;
-        while (l<N && r<N){
-            last_cow = c[r].type;
-            if (prev_cow!='-' && last_cow!=prev_cow){
-                if (last_cow == 'W'){
-                    last_cow = 'S';
+        int[] prefix = new int[N];
+        Arrays.sort(a);
+        for (int i = 0; i<N; i++){
+            if (Objects.equals(h.get(a[i]), "W")){
+                if (i == 0){
+                    prefix[0] = 1;
+                }else{
+                    prefix[i] = prefix[i-1]+1;
                 }
             }else{
-                if (last_cow == 'S'){
-                    //change
-                    l = r;
+                if (i == 0){
+                    prefix[0] = -1;
+                }else{
+                    prefix[i] = prefix[i-1]-1;
                 }
             }
-            prev_cow = last_cow;
-            System.out.println(c[l].index+" --> "+c[r].index);
-            if (c[r].index-c[l].index > MAX){
-                MAX = c[r].index-c[l].index;
-            }
-            r++;
+        }
+
+        ArrayList<Integer> final_ = new ArrayList<>();
+
+
+        boolean still_bigger
+        for (int i = 0; i<N; i++){
 
         }
+
+
 
 
 
 
         //Return BEST
         PrintWriter pw = new PrintWriter("fairphoto.out");
-        if (MAX == 0){
-            MAX = 1;
-        }
-        pw.println(MAX);
+
+        pw.println(0);
         pw.close();
 
     }
--- src/Moocast.java
+++ src/Moocast.java
@@ -1,31 +1,44 @@
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
 //import java.util.*;
 //
 //class Node{
 //    int x;
 //    int y;
+//    ArrayList<Node> neighbours = new ArrayList<>();
 //}
 //
 //class Graph{
-//    ArrayList<Node> nodes= new ArrayList<Integer>;
+//    ArrayList<Node> nodes= new ArrayList<Node>;
 //    ArrayList<Node> visited = new ArrayList<>();
-//    public void dfs(){
-//        int f =
-//        for (nodes of visited){
-//            dfs(nodes);
+//    public void dfs(Node node){
+//        if (visited.contains(node))
+//        for (Node neighbour : node.neighbours){
+//            dfs(neighbour);
+//
+//
 //        }
 //
 //    }
 //}
 //public class Moocast {
-//    public static void main(String[] args){
+//    public static void main(String[] args) throws IOException {
 //        //4
 //        //1 3
 //        //5 4
 //        //7 2
 //        //6 1
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(br.readLine());
+//        Graph g = new Graph();
+//        for (int i = 0; i<N; i++){
+//            String[] s = br.readLine().split(" ");
+//            g.nodes.add(new Node(Integer.parseInt(s[m])))
+//        }
 //
 //
-//        System.out.println(Graph.dfs());
+//        System.out.println(g.dfs(Node g.starting));
 //
 //
 //
