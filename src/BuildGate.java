7a14da6ca3fa13cd72d34f737868a52a68074d20
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-07-16 16:50:00 -0700

    merged a few functions together

--- src/BuildGate.java
+++ src/BuildGate.java
@@ -1,49 +1,4 @@
-import java.util.ArrayList;
-
-class TheFloodfill {
-    private static int[][] grid;  // the grid itself
-    private static int rowNum;
-    private static int colNum;  // grid dimensions, rows and columns
-    private static boolean[][] visited;  // keeps track of which nodes have been visited
-    private static int currSize = 0;  // reset to 0 each time we start a new component
-
-    public static void main(String[] args) {
-        /*
-         * input code and other problem-specific stuff here
-         */
-        for (int r = 0; r < rowNum; r++){
-            for (int c = 0; c < colNum; c++){
-                if(!visited[r][c]){
-                    currSize = 0;
-                    /*
-                     * start a flood fill if the square hasn't already been visited,
-                     * and then store or otherwise use the component size
-                     * for whatever it's needed for
-                     */
-                    floodfill(r, c, grid[r][c]);
-                }
-            }
-        }
-    }
-
-    private static void floodfill(int r, int c, int color) {
-        if (
-                (r < 0 || r >= rowNum || c < 0 || c >= colNum)  // if out of bounds
-                        || grid[r][c] != color  // wrong color
-                        || visited[r][c]  // already visited this square
-        ) return;
-
-        visited[r][c] = true; // mark current square as visited
-        currSize++; // increment the size for each square we visit
-
-        // recursively call flood fill for neighboring squares
-        floodfill(r, c + 1, color);
-        floodfill(r, c - 1, color);
-        floodfill(r - 1, c, color);
-        floodfill(r + 1, c, color);
-    }
-}
-
+import java.util.*;
 
 public class BuildGate {
     public static void println(String[][] arr){
@@ -56,18 +11,54 @@ public class BuildGate {
             System.out.println(line);
         }
     }
+
+    public static void start_floodfill(ArrayList<ArrayList<Integer>> indexes) {
+        int[][] grid = new int[100000][100000];  // the grid itself
+        int rowNum = 10000;
+        int colNum = 10000;  // grid dimensions, rows and columns
+        boolean[][] visited = new boolean[10000][1000];  // keeps track of which nodes have been visited
+        int currSize = 0;  // reset to 0 each time we start a new component
+        for (int r = 0; r < rowNum; r++) {
+            for (int c = 0; c < colNum; c++) {
+                if (!visited[r][c]) {
+                    currSize = 0;
+                    /*
+                     * start a flood fill if the square hasn't already been visited,
+                     * and then store or otherwise use the component size
+                     * for whatever it's needed for
+                     */
+                    floodfill(r, c, grid[r][c], rowNum, colNum, grid, visited, currSize);
+                }
+            }
+        }
+    }
+    public static void floodfill(int r, int c, int color, int rowNum, int colNum, int[][] grid, boolean[][] visited, int currSize) {
+        if (
+                (r < 0 || r >= rowNum || c < 0 || c >= colNum)  // if out of bounds
+                        || grid[r][c] != color  // wrong color
+                        || visited[r][c]  // already visited this square
+        ) return;
+
+        visited[r][c] = true; // mark current square as visited
+        currSize++; // increment the size for each square we visit
+
+        // recursively call flood fill for neighboring squares
+        floodfill(r, c + 1, color, rowNum, colNum, grid, visited, currSize);
+        floodfill(r, c - 1, color, rowNum, colNum, grid, visited, currSize);
+        floodfill(r - 1, c, color, rowNum, colNum, grid, visited, currSize);
+        floodfill(r + 1, c, color, rowNum, colNum, grid, visited, currSize);
+    }
     public static void main(String[] args){
+
         String s = "NNNESWWWSSEEEE";
         ArrayList<ArrayList<Integer>> went = new ArrayList<>();
         int x = 0;
         int y = 0;
 
         ArrayList<Integer> t  = new ArrayList<>();
-        t.add(0);
-        t.add(0);
+        t.add(1000);
+        t.add(1000);
         went.add(t);
-        String[][] grid = new String[20][20];
-        int count = 0;
         for (int i = 0; i<s.toCharArray().length; i++){
             if (s.toCharArray()[i] == 'N'){
                 y++;
@@ -92,22 +83,13 @@ public class BuildGate {
             t.add(y);
             went.add(t);
         }
-        //use pixel thingyyÿ
-//        for (int i = 0; i<grid.length; i++){
-//            for (int j = 0; j<grid[i].length; j++){
-//                grid[i][j] = ".";
-//            }
-//        }
-//        for (int i = 0; i<went.size(); i++){
-//            x = went.get(i).get(0)+10;
-//            y = went.get(i).get(1)+10;
-//            grid[x][y] = "#";
-//        }
-//       println(grid);
 
-        Floodfill(0, 0)
+        start_floodfill(went);
 
     }
+
+    }
+
 
 
 }
