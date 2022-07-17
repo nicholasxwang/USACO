0c226874fe4893819560994a934bc2880250dc00
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-07-17 16:14:33 -0700

    fixd stuff

--- src/BuildGate.java
+++ src/BuildGate.java
@@ -1,106 +1,102 @@
 import java.util.*;
 import java.io.*;
 
-class DetectWall {
-    boolean north;
-    boolean west;
-    boolean south;
-    boolean east;
-    public DetectWall(boolean north, boolean south, boolean east, boolean west) {
-        this.north = north;
-        this.south = south;
-        this.east = east;
-        this.west = west;
+class Point{
+    int x;
+    int y;
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
     }
-    public void change(boolean north, boolean south, boolean east, boolean west) {
-        this.north = north;
-        this.south = south;
-        this.east = east;
-        this.west = west;
-    }
-
 }
 public class BuildGate {
-
-    public static int start_floodfill(int num, ArrayList<ArrayList<Integer>> indexes) {
-        int[][] grid = new int[num*2][num*2];  // the grid itself
-        DetectWall[][] grid2 = new DetectWall[num*2][num*2];  // the grid itself
-        int rowNum = num*2;
-        int colNum = num*2;  // grid dimensions, rows and columns
-        boolean[][] visited = new boolean[num*2][num*2];  // keeps track of which nodes have been visited
-        for (int i = 0; i<grid2.length; i++) {
-            for (int j = 0; j < grid2[i].length; j++) {
-                grid2[i][j] = new DetectWall(false, false, false, false);
+    public static void printgrid(String[][] grid){
+        for (int i = 0; i<grid.length; i++){
+            for (int j = 0; j<grid[0].length; j++){
+                System.out.print(grid[i][j]);
             }
+            System.out.println();
         }
-        for (int i =0; i<indexes.size(); i++) {
-            int x = indexes.get(i).get(0);
-            int y = indexes.get(i).get(1);
-            grid2[x][y + 1].change(true, grid2[x + 1][y].south, grid2[x + 1][y].east, grid2[x + 1][y].west);
-            grid2[x][y - 1].change(grid2[x + 1][y].north, true, grid2[x + 1][y].east, grid2[x + 1][y].west);
-            grid2[x - 1][y].change(grid2[x + 1][y].north, grid2[x + 1][y].south, true, grid2[x + 1][y].west);
-            grid2[x + 1][y].change(grid2[x + 1][y].north, grid2[x + 1][y].south, grid2[x + 1][y].east, true);
-        }
-        ArrayList<ArrayList<ArrayList<Integer>>> connected = new ArrayList<>(); // Array of Regions of Coordinates of X/Y
-        int ans = 0;
-        for (int r = 0; r < rowNum; r++) {
-            for (int c = 0; c < colNum; c++) {
-                if (!visited[r][c]) {
-                    ans++;
-                    floodfill(r, c, grid2, rowNum, colNum, grid, visited, connected, -1);
-                }
-            }
-        }
-
-        return  ans-1;
-
-
     }
-    public static void floodfill(int r, int c, DetectWall[][] grid2, int rowNum, int colNum, int[][] grid, boolean[][] visited, ArrayList<ArrayList<ArrayList<Integer>>> component, int component_id) {
-        int[] R_CHANGE = {0, 1, 0, -1};
-        int[] C_CHANGE = {1, 0, -1, 0};
-        Stack<ArrayList<Integer>> frontier = new Stack<>();
-        ArrayList<Integer> temp = new ArrayList<Integer>();
-        temp.add(r);
-        temp.add(c);
-        frontier.push(temp);
-        while (!frontier.isEmpty()) {
-            ArrayList<Integer> curr = frontier.pop();
-            int row = curr.get(0);
-            int col = curr.get(1);
+    // Function that returns true if
+    // the given pixel is valid
+    static boolean isValid(String[][] screen, int m, int n, int x, int y, String prevC, String newC)
+    {
+        if(x < 0 || x >= m || y < 0 || y >= n || screen[x][y] != prevC
+                || screen[x][y]== newC || screen[x][y] == "#")
+            return false;
+        return true;
+    }
 
-            if (row < 0 || row >= rowNum || col < 0 || col >= colNum
-                    || visited[row][col])
-                continue;
 
-            visited[row][col] = true;
-            boolean[] dw = {grid2[r][c].north, grid2[r][c].east, grid2[r][c].south, grid2[r][c].west};
-            for (int i = 0; i < 4; i++) {
-                if (dw[i])
-                    continue;
-                temp.clear();
-                temp.add(row + R_CHANGE[i]);
-                temp.add(col + C_CHANGE[i]);
-                frontier.add(temp);
+    // FloodFill function
+    static void floodFill(String[][] screen, int m, int n, int x, int y, String prevC, String newC)
+    {
+        Vector<Point> queue = new Vector<Point>();
+
+        // Append the position of starting
+        // pixel of the component
+        queue.add(new Point(x, y));
+
+        // Color the pixel with the new color
+        screen[x][y] = newC;
+
+        // While the queue is not empty i.e. the
+        // whole component having prevC color
+        // is not colored with newC color
+        while(queue.size() > 0)
+        {
+            // Dequeue the front node
+            Point currPixel = queue.get(queue.size() - 1);
+            queue.remove(queue.size() - 1);
+
+            int posX = currPixel.x;
+            int posY = currPixel.y;
+
+            // Check if the adjacent
+            // pixels are valid
+            if(isValid(screen, m, n, posX + 1, posY, prevC, newC))
+            {
+                // Color with newC
+                // if valid and enqueue
+                screen[posX + 1][posY] = newC;
+                queue.add(new Point(posX + 1, posY));
             }
-            System.out.println(frontier);
 
+            if(isValid(screen, m, n, posX-1, posY, prevC, newC))
+            {
+                screen[posX-1][posY]= newC;
+                queue.add(new Point(posX-1, posY));
+            }
+
+            if(isValid(screen, m, n, posX, posY + 1, prevC, newC))
+            {
+                screen[posX][posY + 1]= newC;
+                queue.add(new Point(posX, posY + 1));
+            }
+
+            if(isValid(screen, m, n, posX, posY-1, prevC, newC))
+            {
+                screen[posX][posY-1]= newC;
+                queue.add(new Point(posX, posY-1));
+            }
         }
-
     }
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new FileReader("gates.in"));
-        int num = Integer.parseInt(br.readLine());
+        br.readLine();
         String s = br.readLine();
         ArrayList<ArrayList<Integer>> went = new ArrayList<>();
-        int x = num;
-        int y = num;
+        int x = 0;
+        int y = 0;
 
         ArrayList<Integer> t  = new ArrayList<>();
-        t.add(x);
-        t.add(x);
+        t.add(0);
+        t.add(0);
         went.add(t);
+        int smallest_x = 10000;
+        int smallest_y = 10000;
         for (int i = 0; i<s.toCharArray().length; i++){
             if (s.toCharArray()[i] == 'N'){
                 y++;
@@ -119,18 +115,89 @@ public class BuildGate {
                 if (went.get(j).get(0) == x && went.get(j).get(1) == y){
                 }
             }
-            t.add(x);
-            t.add(y);
+            t.add(x*2);
+            t.add(y*2);
             went.add(t);
+            //t.clear();
+            ArrayList<Integer> t2  = new ArrayList<>();
+            if (s.toCharArray()[i] == 'N'){
+                t2.add(x*2);
+                t2.add(y*2-1);
+            }
+            if (s.toCharArray()[i] == 'S'){
+                t2.add(x*2);
+                t2.add(y*2+1);
+            }
+            if (s.toCharArray()[i] == 'E'){
+                t2.add(x*2-1);
+                t2.add(y*2);
+            }
+            if (s.toCharArray()[i] == 'W'){
+                t2.add(x*2+1);
+                t2.add(y*2);
+            }
+//            went.add(t);
+            went.add(t2);
+            if (x*2<smallest_x){
+                smallest_x = x*2;
+            }
+            if (y*2<smallest_y){
+                smallest_y = y*2;
+            }
+
         }
 
-        PrintWriter pw = new PrintWriter("gates.out");
-        pw.println(start_floodfill(num, went));
-        pw.close();
+        int x_bound = -1;
+        int y_bound = -1;
 
+        for (int i = 0; i<went.size(); i++){
+            if (smallest_x < 0){
+                went.get(i).set(0,went.get(i).get(0)+(-smallest_x));
+            }
+            if (smallest_y < 0){
+                went.get(i).set(1,went.get(i).get(1)+(-smallest_y));
+            }
+            if (went.get(i).get(0)> x_bound){
+                x_bound = went.get(i).get(0);
+            }
+            if (went.get(i).get(1)> y_bound){
+                y_bound = went.get(i).get(1);
+            }
+        }
+        x_bound++;
+        y_bound++;
+        String[][] grid = new String[x_bound][y_bound];
+        for (int i = 0; i<x_bound; i++){
+            for (int j = 0; j<y_bound; j++){
+                grid[i][j] = ".";
+            }
+        }
+        for (int i = 0; i<went.size(); i++){
+            grid[went.get(i).get(0)][went.get(i).get(1)] = "#";
+        }
+        //printgrid(grid);
+        int ans = -1;
+        for (int i = 0; i<grid.length; i++){
+            for (int j = 0; j<grid[i].length; j++){
+                if (grid[i][j] == "#")
+                    continue;
+                if (grid[i][j] == "•")
+                    continue;
+                floodFill(grid,grid.length, grid[0].length, i, j, grid[x][y], "•");
+                printgrid(grid);
+                System.out.println("――――――――――――――――――――――――――――――");
+                ans++;
+            }
+        }
+        //printgrid(grid);
+        System.out.println(ans);
+        PrintWriter pw = new PrintWriter("gates.out");
+        pw.close();
     }
 
 }
+
+
 
 
 
--- src/Moocast_Silver.java
+++ src/Moocast_Silver.java
@@ -1,93 +1,107 @@
-import java.io.*;
-import java.util.*;
-
-class Node{
-    int x;
-    int y;
-    int P;
-    int id;
-    ArrayList<Node> neighbours;
-    public Node(int x, int y, int P, ArrayList<Node> neighbours, int id){
-        this.x = x;
-        this.y = y;
-        this.P = P;
-        this.neighbours = neighbours;
-        this.id = id;
-    }
-}
-
-class Graph{
-    ArrayList<Node> nodes;
-    public Graph(ArrayList<Node> nodes) {
-        this.nodes = nodes;
-    }
-    public static boolean Pythagorean(int x_1, int y_1, int x_2, int y_2, int P){
-        return (P*P > (x_1-x_2)*(x_1-x_2) + (y_1-y_2)*(y_1-y_2));
-    }
-    public void add_node(int x, int y, int P, int id){
-        Node n = new Node(x,y,P, new ArrayList<Node>(), id);
-        for (int i = 0; i<this.nodes.size(); i++){
-            if (Pythagorean(x, y, this.nodes.get(i).x, this.nodes.get(i).y, P)){
-                n.neighbours.add(this.nodes.get(i));
-            }
-            if (Pythagorean(x, y, this.nodes.get(i).x, this.nodes.get(i).y, this.nodes.get(i).P)){
-                this.nodes.get(i).neighbours.add(n);
-            }
-        }
-        this.nodes.add(n);
-    }
-    public int start_dfs(){
-        int[] cache = new int[nodes.size()];
-        for (int i = 0; i<cache.length; i++){
-            cache[i] = -1;
-        }
-        int ans = 0;
-        for (int i = 0; i<nodes.size(); i++){
-            int dfs;
-            if (cache[nodes.get(i).id]!= -1){
-                dfs = cache[nodes.get(i).id];
-            }else{
-                dfs = dfs(nodes.get(i), cache);
-            }
-            if (dfs > ans)
-                ans = dfs(nodes.get(i), cache);
-        }
-        return ans;
-
-    }
-    public int dfs(Node n, int[] cache){
-        int ans = 1;
-        for (int i = 0; i<n.neighbours.size(); i++){
-            int dfs;
-            if (cache[n.neighbours.get(i).id]!=-1){
-                dfs = cache[n.neighbours.get(i).id];
-            }else{
-                dfs = dfs(n.neighbours.get(i), cache);
-                cache[n.neighbours.get(i).id] = dfs;
-            }
-
-            ans += dfs;
-        }
-        return ans;
-    }
-
-    }
-
-
-
-public class Moocast_Silver {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new FileReader("moocast.in"));
-        int N = Integer.parseInt(br.readLine());
-        Graph g = new Graph(new ArrayList<Node>());
-        String[] t;
-        for (int i = 0; i<N; i++){
-            t = br.readLine().split(" ");
-            g.add_node(Integer.parseInt(t[0]),Integer.parseInt(t[1]),Integer.parseInt(t[2]), i);
-
-        }
-        PrintWriter pw = new PrintWriter("moocast.out");
-        pw.println(g.start_dfs());
-        pw.close();
-    }
-}
+//import java.io.*;
+//import java.util.*;
+//
+//class Node{
+//    int x;
+//    int y;
+//    int P;
+//    int id;
+//    ArrayList<Node> neighbours;
+//    public Node(int x, int y, int P, ArrayList<Node> neighbours, int id){
+//        this.x = x;
+//        this.y = y;
+//        this.P = P;
+//        this.neighbours = neighbours;
+//        this.id = id;
+//    }
+//}
+//
+//class Graph{
+//    ArrayList<Node> nodes;
+//    public Graph(ArrayList<Node> nodes) {
+//        this.nodes = nodes;
+//    }
+//    public static boolean Pythagorean(int x_1, int y_1, int x_2, int y_2, int P){
+//        return (P*P > (x_1-x_2)*(x_1-x_2) + (y_1-y_2)*(y_1-y_2));
+//    }
+//    public void add_node(int x, int y, int P, int id){
+//        Node n = new Node(x,y,P, new ArrayList<Node>(), id);
+//        for (int i = 0; i<this.nodes.size(); i++){
+//            if (Pythagorean(x, y, this.nodes.get(i).x, this.nodes.get(i).y, P)){
+//                n.neighbours.add(this.nodes.get(i));
+//            }
+//            if (Pythagorean(x, y, this.nodes.get(i).x, this.nodes.get(i).y, this.nodes.get(i).P)){
+//                this.nodes.get(i).neighbours.add(n);
+//            }
+//        }
+//        this.nodes.add(n);
+//    }
+//    public int start_dfs(){
+//        int[] cache = new int[nodes.size()];
+//        for (int i = 0; i<cache.length; i++){
+//            cache[i] = -1;
+//        }
+//        int ans = 0;
+//        for (int i = 0; i<nodes.size(); i++){
+//            int dfs;
+//            if (cache[nodes.get(i).id]!= -1){
+//                dfs = cache[nodes.get(i).id];
+//            }else{
+//                dfs = dfs(nodes.get(i), cache);
+//            }
+//            if (dfs > ans)
+//                ans = dfs(nodes.get(i), cache);
+//        }
+//        return ans;
+//
+//    }
+//    public int dfs(Node n, int[] cache...){
+//        int ans = 1;
+//        for (int i = 0; i<n.neighbours.size(); i++){
+//            int dfs;
+//            if (cache[n.neighbours.get(i).id]!=-1){
+//                dfs = cache[n.neighbours.get(i).id];
+//            }else{
+//                dfs = dfs(n.neighbours.get(i), cache);
+//                cache[n.neighbours.get(i).id] = dfs;
+//            }
+//
+//            ans += dfs;
+//        }
+//        return ans;
+//    }
+//
+//    }
+//
+//
+//
+//public class Moocast_Silver {
+//    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new FileReader("moocast.in"));
+//        int N = Integer.parseInt(br.readLine());
+//        Graph g = new Graph(new ArrayList<Node>());
+//        String[] t;
+//        for (int i = 0; i<N; i++){
+//            t = br.readLine().split(" ");
+//            g.add_node(Integer.parseInt(t[0]),Integer.parseInt(t[1]),Integer.parseInt(t[2]), i);
+//
+//        }
+//        boolean[][] canTransmit = new boolean[N][N];
+//        PrintWriter pw = new PrintWriter("moocast.out");
+//        pw.println(g.start_dfs());
+//        pw.close();
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
