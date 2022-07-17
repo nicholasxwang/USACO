d6da9e61074a0dd5bf45e374f5a2bf3a92c019d6
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-07-17 10:26:29 -0700

    got #1 working

--- src/BuildGate.java
+++ src/BuildGate.java
@@ -32,11 +32,11 @@ public class BuildGate {
     }
 
     public static void start_floodfill(ArrayList<ArrayList<Integer>> indexes) {
-        int[][] grid = new int[40][40];  // the grid itself
-        DetectWall[][] grid2 = new DetectWall[40][40];  // the grid itself
-        int rowNum = 40;
-        int colNum = 40;  // grid dimensions, rows and columns
-        boolean[][] visited = new boolean[40][40];  // keeps track of which nodes have been visited
+        int[][] grid = new int[10][10];  // the grid itself
+        DetectWall[][] grid2 = new DetectWall[10][10];  // the grid itself
+        int rowNum = 10;
+        int colNum = 10;  // grid dimensions, rows and columns
+        boolean[][] visited = new boolean[10][10];  // keeps track of which nodes have been visited
         for (int i = 0; i<grid2.length; i++) {
             for (int j = 0; j < grid2[i].length; j++) {
                 grid2[i][j] = new DetectWall(false, false, false, false);
@@ -47,30 +47,26 @@ public class BuildGate {
             int y = indexes.get(i).get(1);
             grid2[x][y + 1].change(true, grid2[x + 1][y].south, grid2[x + 1][y].east, grid2[x + 1][y].west);
             grid2[x][y - 1].change(grid2[x + 1][y].north, true, grid2[x + 1][y].east, grid2[x + 1][y].west);
-            grid2[x + 1][y].change(grid2[x + 1][y].north, grid2[x + 1][y].south, true, grid2[x + 1][y].west);
-            grid2[x - 1][y].change(grid2[x + 1][y].north, grid2[x + 1][y].south, grid2[x + 1][y].east, true);
+            grid2[x - 1][y].change(grid2[x + 1][y].north, grid2[x + 1][y].south, true, grid2[x + 1][y].west);
+            grid2[x + 1][y].change(grid2[x + 1][y].north, grid2[x + 1][y].south, grid2[x + 1][y].east, true);
         }
         ArrayList<ArrayList<ArrayList<Integer>>> connected = new ArrayList<>(); // Array of Regions of Coordinates of X/Y
         for (int r = 0; r < rowNum; r++) {
             for (int c = 0; c < colNum; c++) {
                 if (!visited[r][c]) {
-                    System.out.println("++++++");
+                   // System.out.println("++++++");
                     floodfill(r, c, grid2, rowNum, colNum, grid, visited, connected, -1);
                 }
             }
-            System.out.println(connected);
+            //System.out.println(connected);
         }
+        System.out.println(connected.size()-1);
 
 
     }
     public static void floodfill(int r, int c, DetectWall[][] grid2, int rowNum, int colNum, int[][] grid, boolean[][] visited, ArrayList<ArrayList<ArrayList<Integer>>> component, int component_id) {
-        System.out.println("RECURSIVE ("+r+", "+c+")");
-        if (r== 20 && c==22){
-            System.out.println("here");
-        }
-        if (r== 21 && c==22){
-            System.out.println("here");
-        }
+        //System.out.println("RECURSIVE ("+r+", "+c+")");
+
         if (
                 (r < 0 || r >= rowNum || c < 0 || c >= colNum)  // if out of bounds
                         // wrong color
@@ -102,12 +98,12 @@ public class BuildGate {
 
         String s = "NNNESWWWSSEEEE";
         ArrayList<ArrayList<Integer>> went = new ArrayList<>();
-        int x = 20;
-        int y = 20;
+        int x = 5;
+        int y = 5;
 
         ArrayList<Integer> t  = new ArrayList<>();
-        t.add(20);
-        t.add(20);
+        t.add(5);
+        t.add(5);
         went.add(t);
         for (int i = 0; i<s.toCharArray().length; i++){
             if (s.toCharArray()[i] == 'N'){
@@ -122,11 +118,11 @@ public class BuildGate {
             if (s.toCharArray()[i] == 'W'){
                 x--;
             }
-            System.out.println("("+x+", "+y+")");
+            //System.out.println("("+x+", "+y+")");
             t  = new ArrayList<>();
             for (int j = 0; j<went.size(); j++){
                 if (went.get(j).get(0) == x && went.get(j).get(1) == y){
-                    System.out.println("DUPLICATE!!!");
+                    //System.out.println("DUPLICATE!!!");
                 }
             }
             t.add(x);
