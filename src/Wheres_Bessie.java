b27589df74b09c9d856528aa7d04c126a490221a
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-07-18 22:44:17 -0700

    k

--- src/Wheres_Bessie.java
+++ src/Wheres_Bessie.java
@@ -1,20 +1,20 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Hashtable;
 import java.util.Vector;
 
 
 public class Wheres_Bessie {
-    static boolean isValid(String[][] screen, int m, int n, int x, int y, String prevC, String newC)
-    {
-        if(x < 0 || x >= m || y < 0 || y >= n
-                || screen[x][y]== newC || screen[x][y] == "#")
+    static boolean isValid(String[][] screen, int m, int n, int x, int y, String prevC, String newC) {
+        if (x < 0 || x >= m || y < 0 || y >= n
+                || screen[x][y] == newC || screen[x][y] == "#")
             return false;
         return true;
     }
 
 
     // FloodFill function
-    static void floodFill(String[][] screen, int m, int n, int x, int y, String prevC, String newC)
-    {
+    static void floodFill(String[][] screen, int m, int n, int x, int y, String prevC, String newC) {
         Vector<Point> queue = new Vector<Point>();
 
         // Append the position of starting
@@ -27,8 +27,7 @@ public class Wheres_Bessie {
         // While the queue is not empty i.e. the
         // whole component having prevC color
         // is not colored with newC color
-        while(queue.size() > 0)
-        {
+        while (queue.size() > 0) {
             // Dequeue the front node
             Point currPixel = queue.get(queue.size() - 1);
             queue.remove(queue.size() - 1);
@@ -38,71 +37,98 @@ public class Wheres_Bessie {
 
             // Check if the adjacent
             // pixels are valid
-            if(isValid(screen, m, n, posX + 1, posY, prevC, newC))
-            {
+            if (isValid(screen, m, n, posX + 1, posY, prevC, newC)) {
                 // Color with newC
                 // if valid and enqueue
                 screen[posX + 1][posY] = newC;
                 queue.add(new Point(posX + 1, posY));
             }
 
-            if(isValid(screen, m, n, posX-1, posY, prevC, newC))
-            {
-                screen[posX-1][posY]= newC;
-                queue.add(new Point(posX-1, posY));
+            if (isValid(screen, m, n, posX - 1, posY, prevC, newC)) {
+                screen[posX - 1][posY] = newC;
+                queue.add(new Point(posX - 1, posY));
             }
 
-            if(isValid(screen, m, n, posX, posY + 1, prevC, newC))
-            {
-                screen[posX][posY + 1]= newC;
+            if (isValid(screen, m, n, posX, posY + 1, prevC, newC)) {
+                screen[posX][posY + 1] = newC;
                 queue.add(new Point(posX, posY + 1));
             }
 
-            if(isValid(screen, m, n, posX, posY-1, prevC, newC))
-            {
-                screen[posX][posY-1]= newC;
-                queue.add(new Point(posX, posY-1));
+            if (isValid(screen, m, n, posX, posY - 1, prevC, newC)) {
+                screen[posX][posY - 1] = newC;
+                queue.add(new Point(posX, posY - 1));
             }
         }
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         //4
         //ABBC
         //BBBC
         //AABB
         //ABBC
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter("");
+        //PrintWriter pw = new PrintWriter("");
         int N = Integer.parseInt(br.readLine());
         String[][] grid = new String[N][N];
         String[] s;
-        for (int i = 0; i<N; i++){
-            s = br.readLine().split(" ");
-            for (int j = 0; j<N; j++){
+        for (int i = 0; i < N; i++) {
+            s = br.readLine().split("");
+            for (int j = 0; j < N; j++) {
                 grid[i][j] = s[j];
             }
         }
-
-        for (int i = 0; i<N; i++){
-            for (int j = 0; j<N; j++){
-                for (int k = 0; k<N; k++){
-                    for (int l = 0; l<N; l++){
-                        String[][] newgrid = new String[j-i][k-j];
-                        for (int a = i; a<j; a++){
-                            for (int b = k; b<l; b++){
-                                newgrid[j-i][l-k] = grid[a][b];
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    for (int l = k+1; l < N; l++) {
+                        System.out.println("("+i+", "+j+", "+k+", "+l+")");
+                        if (i== 0 && j==3 && k==0 && l == 1){
+                            System.out.println();
+                        }
+                        String[][] newgrid = new String[j - i][l-k];
+                        for (int a = i; a < j; a++) {
+                            for (int b = k; b < l; b++) {
+                                newgrid[j - i-1][l - k-1] = grid[a][b];
                             }
                         }
-                        for (int a = 0; i<newgrid.length; i++){
-                            for (int b = 0; j<newgrid[i].length; j++){
-                                //floodFill(floodFill(floodFill(floodFill(floodFill(floodFill(floodFill(floodFill(floodFill(floodFill(floodFill(floodFill(5, 5, 5, ,5););););););););););););
-                                if (grid[i][j] == "#")
-
-                                if (grid[i][j] == "•")
+                        for (int a = 0; i < newgrid.length; i++) {
+                            boolean breaky = false;
+                            for (int b = 0; j < newgrid[i].length; j++) {
+                                Hashtable<String, Integer> ht = new Hashtable<>();
+                                String current = newgrid[a][b];
+                                if (current == "#")
                                     continue;
-                                floodFill(grid,grid.length, grid[0].length, i, j, ".", "•");
+                                floodFill(grid, grid.length, grid[0].length, i, j, current, "#");
+                                if (!ht.keySet().contains(current)){
+                                    ht.put(current, 0);
+                                }
+                                ht.put(current, ht.get(current)+1);
+                                if (ht.size() > 2){
+                                    breaky = true;
+                                    break;
+                                }
+                                boolean returned = false;
+                                boolean returned2 = false;
+                                for (String key : ht.keySet()){
+                                    if (ht.get(key) == 1){
+                                        returned = true;
+                                    }else{
+                                        returned2 = true;
+                                    }
+                                }
+                                if (!returned && !returned2){
+                                    breaky = true;
+                                    break;
+                                }
 
+                            }
+                            if (breaky){
+                                break;
+                            }else{
+                                ans++;
+                                System.out.println(Arrays.deepToString(newgrid));
                             }
                         }
 
@@ -110,7 +136,7 @@ public class Wheres_Bessie {
                 }
             }
         }
-
+        System.out.println(ans);
 
     }
 }
--- src/WhyDidTheCowCrossTheRoad.java
+++ src/WhyDidTheCowCrossTheRoad.java
@@ -1,51 +1,77 @@
-import java.io.*;
-import java.util.*;
-
-class Coordinate{
-    int x;
-    int y;
-    public Coordinate(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
-public class WhyDidTheCowCrossTheRoad {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter("");
-        ArrayList<Coordinate> cows = new ArrayList<>();
-        ArrayList<ArrayList<Coordinate>> roads = new ArrayList<>();
-        //3 3 3
-        //2 2 2 3
-        //3 3 3 2
-        //3 3 2 3
-        //3 3
-        //2 2
-        //2 3
-        String[] s = br.readLine().split(" ");
-        int N = Integer.parseInt(s[0]);
-        int K = Integer.parseInt(s[1]);
-        int R = Integer.parseInt(s[2]);
-        for (int i = 0; i<K; i++){
-            s = br.readLine().split(" ");
-            Coordinate c1 = new Coordinate(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-            Coordinate c2 = new Coordinate(Integer.parseInt(s[2]), Integer.parseInt(s[3]));
-            ArrayList<Coordinate> road = new ArrayList<>();
-            road.add(c1);
-            road.add(c2);
-            roads.add(road);
-        }
-        for (int i = 0; i<K; i++){
-            s = br.readLine().split(" ");
-            Coordinate c1 = new Coordinate(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-            cows.add(c1);
-        }
-
-
-
-
-
-
-    }
-}
-
+//import java.io.*;
+//import java.util.*;
+//
+//class Coordinate{
+//    int x;
+//    int y;
+//    public Coordinate(int x, int y){
+//        this.x = x;
+//        this.y = y;
+//    }
+//}
+//class cow_Node{
+//    Coordinate c;
+//    int id;
+//    ArrayList<Node> neighbours;
+//    public cow_Node(Coordinate C, int id, ArrayList<Node> neighbours){
+//        this.c = C;
+//        this.id = id;
+//        this.neighbours = neighbours;
+//    }
+//}
+//class cow_Graph{
+//    ArrayList<Node> nodes;
+//    public cow_Graph(ArrayList<Node> nodes){
+//        this.nodes = nodes;
+//    }
+//
+//}
+//public class WhyDidTheCowCrossTheRoad {
+//    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        PrintWriter pw = new PrintWriter("");
+//        ArrayList<Coordinate> cows = new ArrayList<>();
+//        ArrayList<ArrayList<Coordinate>> roads = new ArrayList<>();
+//        //3 3 3
+//        //2 2 2 3
+//        //3 3 3 2
+//        //3 3 2 3
+//        //3 3
+//        //2 2
+//        //2 3
+//        String[] s = br.readLine().split(" ");
+//        int N = Integer.parseInt(s[0]);
+//        int K = Integer.parseInt(s[1]);
+//        int R = Integer.parseInt(s[2]);
+//        for (int i = 0; i<K; i++){
+//            s = br.readLine().split(" ");
+//            Coordinate c1 = new Coordinate(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
+//            Coordinate c2 = new Coordinate(Integer.parseInt(s[2]), Integer.parseInt(s[3]));
+//            ArrayList<Coordinate> road = new ArrayList<>();
+//            road.add(c1);
+//            road.add(c2);
+//            roads.add(road);
+//        }
+//        for (int i = 0; i<K; i++){
+//            s = br.readLine().split(" ");
+//            Coordinate c1 = new Coordinate(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
+//            cows.add(c1);
+//        }
+//        //Don't use floodfill o_O, use bfs
+//
+//        //Find pairs 😀
+//        for (int i = 0; i<cows.size(); i++){
+//            for (int j = i; j<cows.size(); j++){
+//                // Check if pairs :thonk:
+//                for ()
+//            }
+//        }
+//
+//
+//
+//
+//
+//
+//    }
+//}
+//
