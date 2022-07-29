927849f5d5875b685a0409e9a3da77578a1bdbf7
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-07-29 08:49:01 -0700

    stuf

--- src/WhyDidTheCowCrossTheRoad.java
+++ src/WhyDidTheCowCrossTheRoad.java
@@ -1,132 +1,132 @@
-import java.io.*;
-import java.util.*;
-
-class Coordinate{
-    int x;
-    int y;
-    public Coordinate(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-}
-class cow_Node{
-    Coordinate c;
-    int id;
-    ArrayList<Node> neighbours;
-    public cow_Node(Coordinate C, int id, ArrayList<Node> neighbours){
-        this.c = C;
-        this.id = id;
-        this.neighbours = neighbours;
-    }
-}
-class cow_Graph{
-    ArrayList<Node> nodes;
-    public cow_Graph(ArrayList<Node> nodes){
-        this.nodes = nodes;
-    }
-
-}
-public class WhyDidTheCowCrossTheRoad {
-    static boolean isValid(String[][] screen, int m, int n, int x, int y, String prevC, String newC) {
-        if (x < 0 || x >= m || y < 0 || y >= n
-                || screen[x][y].equals(newC) || !screen[x][y].equals(prevC))
-            return false;
-        return true;
-    }
-
-
-    // FloodFill function
-    static void floodFill(String[][] screen, int m, int n, int x, int y, String prevC, String newC) {
-        Vector<Point> queue = new Vector<Point>();
-
-        // Append the position of starting
-        // pixel of the component
-        queue.add(new Point(x, y));
-
-        // Color the pixel with the new color
-        screen[x][y] = newC;
-
-        // While the queue is not empty i.e. the
-        // whole component having prevC color
-        // is not colored with newC color
-        while (queue.size() > 0) {
-            // Dequeue the front node
-            Point currPixel = queue.get(queue.size() - 1);
-            queue.remove(queue.size() - 1);
-
-            int posX = currPixel.x;
-            int posY = currPixel.y;
-
-            // Check if the adjacent
-            // pixels are valid
-            if (isValid(screen, m, n, posX + 1, posY, prevC, newC)) {
-                // Color with newC
-                // if valid and enqueue
-                screen[posX + 1][posY] = newC;
-                queue.add(new Point(posX + 1, posY));
-            }
-
-            if (isValid(screen, m, n, posX - 1, posY, prevC, newC)) {
-                screen[posX - 1][posY] = newC;
-                queue.add(new Point(posX - 1, posY));
-            }
-
-            if (isValid(screen, m, n, posX, posY + 1, prevC, newC)) {
-                screen[posX][posY + 1] = newC;
-                queue.add(new Point(posX, posY + 1));
-            }
-
-            if (isValid(screen, m, n, posX, posY - 1, prevC, newC)) {
-                screen[posX][posY - 1] = newC;
-                queue.add(new Point(posX, posY - 1));
-            }
-        }
-    }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter("");
-        ArrayList<Coordinate> cows = new ArrayList<>();
-        ArrayList<ArrayList<Coordinate>> roads = new ArrayList<>();
-        //3 3 3
-        //2 2 2 3
-        //3 3 3 2
-        //3 3 2 3
-        //3 3
-        //2 2
-        //2 3
-        String[] s = br.readLine().split(" ");
-        int N = Integer.parseInt(s[0]);
-        int K = Integer.parseInt(s[1]);
-        int R = Integer.parseInt(s[2]);
-        for (int i = 0; i<K; i++){
-            s = br.readLine().split(" ");
-            Coordinate c1 = new Coordinate(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-            Coordinate c2 = new Coordinate(Integer.parseInt(s[2]), Integer.parseInt(s[3]));
-            ArrayList<Coordinate> road = new ArrayList<>();
-            road.add(c1);
-            road.add(c2);
-            roads.add(road);
-        }
-        for (int i = 0; i<K; i++){
-            s = br.readLine().split(" ");
-            Coordinate c1 = new Coordinate(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
-            cows.add(c1);
-        }
-        //Don't use floodfill o_O, use bfs
-
-        //Find pairs 😀
-        for (int i = 0; i<cows.size(); i++){
-            for (int j = i; j<cows.size(); j++){
-                // Check if pairs :thonk:
-                for ()
-            }
-        }
-
-
-
-
-
-
-    }
-}
-
+//import java.io.*;
+//import java.util.*;
+//
+//class Coordinate{
+//    int x;
+//    int y;
+//    public Coordinate(int x, int y){
+//        this.x = x;
+//        this.y = y;
+//    }
+//}
+//class cow_Node{
+//    Coordinate c;
+//    int id;
+//    ArrayList<Node> neighbours;
+//    public cow_Node(Coordinate C, int id, ArrayList<Node> neighbours){
+//        this.c = C;
+//        this.id = id;
+//        this.neighbours = neighbours;
+//    }
+//}
+//class cow_Graph{
+//    ArrayList<Node> nodes;
+//    public cow_Graph(ArrayList<Node> nodes){
+//        this.nodes = nodes;
+//    }
+//
+//}
+//public class WhyDidTheCowCrossTheRoad {
+//    static boolean isValid(String[][] screen, int m, int n, int x, int y, String prevC, String newC) {
+//        if (x < 0 || x >= m || y < 0 || y >= n
+//                || screen[x][y].equals(newC) || !screen[x][y].equals(prevC))
+//            return false;
+//        return true;
+//    }
+//
+//
+//    // FloodFill function
+//    static void floodFill(String[][] screen, int m, int n, int x, int y, String prevC, String newC) {
+//        Vector<Point> queue = new Vector<Point>();
+//
+//        // Append the position of starting
+//        // pixel of the component
+//        queue.add(new Point(x, y));
+//
+//        // Color the pixel with the new color
+//        screen[x][y] = newC;
+//
+//        // While the queue is not empty i.e. the
+//        // whole component having prevC color
+//        // is not colored with newC color
+//        while (queue.size() > 0) {
+//            // Dequeue the front node
+//            Point currPixel = queue.get(queue.size() - 1);
+//            queue.remove(queue.size() - 1);
+//
+//            int posX = currPixel.x;
+//            int posY = currPixel.y;
+//
+//            // Check if the adjacent
+//            // pixels are valid
+//            if (isValid(screen, m, n, posX + 1, posY, prevC, newC)) {
+//                // Color with newC
+//                // if valid and enqueue
+//                screen[posX + 1][posY] = newC;
+//                queue.add(new Point(posX + 1, posY));
+//            }
+//
+//            if (isValid(screen, m, n, posX - 1, posY, prevC, newC)) {
+//                screen[posX - 1][posY] = newC;
+//                queue.add(new Point(posX - 1, posY));
+//            }
+//
+//            if (isValid(screen, m, n, posX, posY + 1, prevC, newC)) {
+//                screen[posX][posY + 1] = newC;
+//                queue.add(new Point(posX, posY + 1));
+//            }
+//
+//            if (isValid(screen, m, n, posX, posY - 1, prevC, newC)) {
+//                screen[posX][posY - 1] = newC;
+//                queue.add(new Point(posX, posY - 1));
+//            }
+//        }
+//    }
+//    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        PrintWriter pw = new PrintWriter("");
+//        ArrayList<Coordinate> cows = new ArrayList<>();
+//        ArrayList<ArrayList<Coordinate>> roads = new ArrayList<>();
+//        //3 3 3
+//        //2 2 2 3
+//        //3 3 3 2
+//        //3 3 2 3
+//        //3 3
+//        //2 2
+//        //2 3
+//        String[] s = br.readLine().split(" ");
+//        int N = Integer.parseInt(s[0]);
+//        int K = Integer.parseInt(s[1]);
+//        int R = Integer.parseInt(s[2]);
+//        for (int i = 0; i<K; i++){
+//            s = br.readLine().split(" ");
+//            Coordinate c1 = new Coordinate(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
+//            Coordinate c2 = new Coordinate(Integer.parseInt(s[2]), Integer.parseInt(s[3]));
+//            ArrayList<Coordinate> road = new ArrayList<>();
+//            road.add(c1);
+//            road.add(c2);
+//            roads.add(road);
+//        }
+//        for (int i = 0; i<K; i++){
+//            s = br.readLine().split(" ");
+//            Coordinate c1 = new Coordinate(Integer.parseInt(s[0]), Integer.parseInt(s[1]));
+//            cows.add(c1);
+//        }
+//        //Don't use floodfill o_O, use bfs
+//
+//        //Find pairs 😀
+//        for (int i = 0; i<cows.size(); i++){
+//            for (int j = i; j<cows.size(); j++){
+//                // Check if pairs :thonk:
+//                for ()
+//            }
+//        }
+//
+//
+//
+//
+//
+//
+//    }
+//}
+//
--- src/irrigation.java
+++ src/irrigation.java
@@ -1,87 +1,187 @@
-import java.io.*;
-import java.util.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-class DisjointSets {
-    int[] parents;  // 0-indexed
-    int[] sizes;
 
-    public DisjointSets(int size) {
-        sizes = new int[size];
-        parents = new int[size];
-        Arrays.fill(sizes, 1);
-        Arrays.fill(parents, -1);
+class Vertex {
+    private String id;
+    private int value;
+    private Vertex parent;
+    private ArrayList<Vertex> listAdj;
+
+    public Vertex(String id, int value, Vertex parent){
+        this.id = id;
+        this.value = value;
+        this.parent = parent;
+        this.listAdj = new ArrayList<Vertex>();
     }
 
-    // finds the "representative" node in a's component
-    public int find(int x) {
-        return parents[x] == -1 ? x : (parents[x] = find(parents[x]));
-    }
+    public String getId(){return this.id;}
+    public int getValue(){return this.value;}
+    public Vertex getParent(){return this.parent;}
+    public ArrayList<Vertex> getListAdj(){return this.listAdj;}
 
-    // returns whether the merge changed connectivity
-    public boolean union(int x, int y) {
-        int xRoot = find(x);
-        int yRoot = find(y);
-        if (xRoot == yRoot) {
-            return false;
-        }
-        if (sizes[xRoot] < sizes[yRoot]) {
-            parents[xRoot] = yRoot;
-            sizes[yRoot] += sizes[xRoot];
-        } else {
-            parents[yRoot] = xRoot;
-            sizes[xRoot] += sizes[yRoot];
-        }
-        return true;
-    }
+    public void setId(String id){this.id = id;}
+    public void setValue(int value){this.value = value;}
+    public void setParent(Vertex parent){this.parent = parent;}
 
-    // returns whether two nodes are in the same connected component
-    public boolean connected(int x, int y) {
-        return find(x) == find(y);
+    @Override
+    public String toString() {
+        return "Vertex [id=" + id + ", value=" + value + ", parent=" + parent + "]";
     }
 }
-public class irrigation {
-    public static HashMap<Integer, ArrayList<Integer>> MST;
-    public static PriorityQueue<Edge> pq; //contains all edges
 
-    public static int calculate(int xi, int xj, int yi, int yj){
-        return (xi-xj)*(xi-xj)+(yi-yj)*(yi-yj);
-    }
-    //Assumes that DSU code is included
-    public static void kruskal() {
-        while (!pq.isEmpty()) {
-            Edge e = pq.poll();
-            if (DisjointSets.find(e.start) != DisjointSets.find(e.end)) {
-                MST.get(e.start).add(e.end);
-                MST.get(e.end).add(e.start);
-                DisjointSets.union(e.start, e.end);
+class irrigation {
+   static int INFINITY = 999999999;
+   public static int calculate(int xi, int yi, int xf, int yf){
+       return (xi-xf)*(xi-xf)+(yi-yf)*(yi-yf);
+   }
+
+    /*PRIM MST*/
+
+    public static void primMST(int sizeV, int[][] matrixAdj, Vertex[] listVertex){
+
+        Vertex[] result = new Vertex[sizeV];
+        Vertex currentVertex = null;
+        int oldSize = 0;
+
+        listVertex[0].setValue(0); //Atribui zero ao vertice inicial
+        /*q eh a lista de vertices a serem investigados*/
+        ArrayList<Vertex> q = buildMinHeap(listVertex.clone());
+
+        while ((oldSize = q.size()) != 0){
+            currentVertex = q.remove(0);		//Remove o menor valor (primeiro vertex da list)
+
+            for (Vertex v : currentVertex.getListAdj()){
+                if(q.contains(v) && matrixAdj[currentVertex.getId()][v.getId()] < v.getValue()){
+                    v.setParent(currentVertex);		//Atribui o vertice retirado como o novo pai
+                    v.setValue(matrixAdj[currentVertex.getId()][v.getId()]);	//Modifica o valor do vertice pelo peso da aresta correspondente
+                }
             }
+
+            q = buildMinHeap(q.toArray(new Vertex[q.size()]));
+
+            result[sizeV - oldSize] = currentVertex;		//Guarda os vertices ja acessados
         }
+
+        int total = 0;
+        System.out.println("Result Set:");
+        for(int i = 0; i < result.length; i++){
+
+            if(result[i].getParent() != null)
+                System.out.println(result[i].getId()+"-"+result[i].getParent().getId()+" (value = "+result[i].getValue()+")");
+            else
+                System.out.println(result[i].getId()+"-"+result[i].getParent()+" (value = "+result[i].getValue()+")");
+
+            total += result[i].getValue();
+        }
+
+        System.out.println("\nTotal Value: "+total);
+
     }
 
-    static class Edge implements Comparable<Edge> {
-        int start,
-                end,
-                weight;
-        public Edge(int s, int e, int w) {
-            start = s;
-            end = e;
-            weight = w;
-        }
-        public int compareTo(Edge o) {
-            if (this.weight > o.weight) {
-                return 1;
-            } else if (this.weight == o.weight) {
-                return 0;
-            } else {
-                return -1;
-            }
-        }
+    /*SUPPORT FUNCTIONS*/
+    /*HEAP MIN*/
+
+    private static ArrayList<Vertex> buildMinHeap(Vertex[] vet){
+
+        for (int i = (vet.length/2)-1; i >= 0; i--)
+            minHeapfy(vet,vet.length,i);
+
+        ArrayList<Vertex> ret = new ArrayList<Vertex>();
+
+        for (int i = 0; i < vet.length; i++)
+            ret.add(vet[i]);
+
+        return ret;
     }
+
+    private static Vertex[] minHeapfy(Vertex[] vet, int n, int index){
+        int min = index, left = 2 * index, right = 2 * index + 1;
+
+        if ((left <= n - 1) && (vet[left].getValue() < vet[min].getValue()))
+            min = left;
+
+        if((right <= n-1) && (vet[right].getValue() < vet[min].getValue()))
+            min = right;
+
+        if(min != index){
+            Vertex aux = vet[index];
+            vet[index] = vet[min];
+            vet[min] = aux;
+
+            return minHeapfy(vet,n,min);
+        }
+
+        return vet;
+    }
+
+    private static void showBiMatrix(int[][] m){
+        for (int[] rows : m) {
+            for (int col : rows) {
+                System.out.format("%5d", col);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    /*
+        Modelo de entrada:
+            n
+            mAdj[i,j]
+
+
+    */
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
 
+        String[] temp;
+        //3 11
+        //0 2
+        //5 0
+        //4 3
+
+        temp = br.readLine().split(" ");
+        int N = Integer.parseInt(temp[0]);
+        int C = Integer.parseInt(temp[1]);
+        temp = br.readLine().split(" ");
+        int init_i = Integer.parseInt(temp[0]);
+        int init_j = Integer.parseInt(temp[1]);
+        int[][] matrixAdj = new int[N][N];
+        Vertex[] listVertex = new Vertex[N];
+        listVertex[0] = new Vertex(init_i + "x" + init_j, INFINITY, null);
+        for (int i = 1; i < N; i++) {
+            temp = br.readLine().split(" ");
+            int cur_i = Integer.parseInt(temp[0]);
+            int cur_j = Integer.parseInt(temp[1]);
+            listVertex[i] = new Vertex(cur_i + "x" + cur_j, INFINITY, null);
+
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (boolean true) {
+                    int value = Integer.parseInt(listVertex[i]);
+                    matrixAdj[i][j] = value;
+                    matrixAdj[j][i] = value;
+
+                    if (value != 0) {
+                        listVertex[i].getListAdj().add(listVertex[j]);
+                        listVertex[j].getListAdj().add(listVertex[i]);
+                    }
+
+                    k++;
+                } else {
+                    j--;
+                    k++;
+                }
+            }
+        }
+        showBiMatrix(matrixAdj);
+        primMST(N, matrixAdj, listVertex);
     }
-
 }
