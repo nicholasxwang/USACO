1901456ad468ae3cb4ab57caf99dc34badfedb64
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-04 11:09:56 -0700

    rv1

--- src/Tractor.java
+++ src/Tractor.java
@@ -1,173 +1,112 @@
 import java.io.*;
 import java.util.*;
-class Location{
-    int x;
-    int y;
-    int id;
-    public Location(int x, int y, int id){
-        this.x = x;
-        this.y = y;
-        this.id = id;
+class Tractor {
+    static int V = 6;
+
+    public static int twod_oned(int x, int y, int size_x, int size_y){
+        return x*size_y + y;
     }
-}
-// Main class DPQ
-public class Tractor {
-    private int dist[];
-    private Set<Integer> settled;
-    private PriorityQueue<DijkstraNode> pq;
-    private int V;
-    List<List<DijkstraNode> > adj;
-
-    public Tractor(int V)
-    {
-
-        // This keyword refers to current object itself
-        this.V = V;
-        dist = new int[V];
-        settled = new HashSet<Integer>();
-        pq = new PriorityQueue<DijkstraNode>(V, new DijkstraNode());
+    public static ArrayList<Integer> oned_twodd(int id, int size_x, int size_y){
+        ArrayList<Integer> ar = new ArrayList<>();
+        ar.add(id/size_y);
+        ar.add(id%size_y);
+        return ar;
     }
 
-    // Method 1
-    // Dijkstra's Algorithm
-    public void dijkstra(List<List<DijkstraNode> > adj, Location src)
-    {
-        this.adj = adj;
-        for (int i = 0; i < V; i++)
-            dist[i] = Integer.MAX_VALUE;
-        pq.add(new DijkstraNode(src, 0));
-        // Distance to the source is 0
-        dist[src.id] = 0;
+    public static boolean includes(ArrayList<ArrayList<Integer>> ar, int x, int y){
+        boolean bool = false;
+        for (int i = 0; i<ar.size(); i++){
+            if (ar.get(i).get(0) == x && ar.get(i).get(1) == y) {bool = true; break;}
 
-        while (settled.size() != V) {
-
-            // Terminating condition check when
-            // the priority queue is empty, return
-            if (pq.isEmpty())
-                return;
-
-            // Removing the minimum distance DijkstraNode
-            // from the priority queue
-            int u = pq.remove().DijkstraNode.id;
-
-            // Adding the DijkstraNode whose distance is
-            // finalized
-            if (settled.contains(u))
-
-                // Continue keyword skips execution for
-                // following check
-                continue;
-
-            // We don't have to call e_Neighbors(u)
-            // if u is already present in the settled set.
-            settled.add(u);
-
-            e_Neighbours(u);
         }
+        return bool;
     }
 
-    // Method 2
-    // To process all the neighbours
-    // of the passed DijkstraNode
-    private void e_Neighbours(int u)
-    {
-
-        int edgeDistance = -1;
-        int newDistance = -1;
-
-        // All the neighbors of v
-
-        for (int i = 0; i < adj.get(u).size(); i++) {
-            DijkstraNode v = adj.get(u).get(i);
-
-            // If current DijkstraNode hasn't already been processed
-            if (!settled.contains(v.DijkstraNode)) {
-                edgeDistance = v.cost;
-                newDistance = dist[u] + edgeDistance;
-
-                // If new distance is cheaper in cost
-                if (newDistance < dist[v.DijkstraNode.id])
-                    dist[v.DijkstraNode.id] = newDistance;
-
-                // Add the current DijkstraNode to the queue
-                pq.add(new DijkstraNode(v.DijkstraNode, dist[v.DijkstraNode.id]));
+    public static int selectMinVertex(ArrayList<Integer> value, ArrayList<Boolean> processed){
+        int minimum = Integer.MAX_VALUE;
+        int vertex = -1;
+        for (int i = 0; i<V; i++){
+            if (!processed.get(i) && value.get(i)<minimum){
+                minimum = value.get(i);
+                vertex = i;
             }
         }
+        return vertex;
     }
+    public static void dijkstra(int graph[][]){
+        int[] parent = new int[V];
+        ArrayList<Integer> value = new ArrayList<>();
+        ArrayList<Boolean> processed = new ArrayList<>();
+        for (int i = 0; i<V; i++){
+            value.add(Integer.MAX_VALUE);
+            processed.add(false);
+        }
+        parent[0] = -1;
+        value.set(0, 0);
+        for (int i = 0; i<V; i++){
+            int U = selectMinVertex(value, processed);
 
-    // Main driver method
-    public static void main(String[] args)  throws IOException {
+            for (int j = 0; j<V; j++){
+                if (graph[U][j] != 0 && !processed.get(j) && value.get(U) != Integer.MAX_VALUE &&
+                value.get(U) + graph[U][j] < value.get(j)){
+                    if (value.get(U) + graph[U][j] < value.get(j)){
+                        value.set(j, value.get(U) + graph[U][j]);
+                        parent[j] = U;
+                    }
+                }
+
+            }
+        }
+        for(int i=1;i<V;++i)
+            System.out.println("U->V: "+parent[i]+"->"+i+"  wt = "+graph[parent[i]][i]+"\n");
+    }
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s =  br.readLine().split(" ");
+        String[] s = br.readLine().split(" ");
         int N = Integer.parseInt(s[0]);
-        int X = Integer.parseInt(s[1]);
-        int Y = Integer.parseInt(s[2]);
-        Location source = new Location(X, Y, 0);
-        // Adjacency list representation of the
-        // connected edges by declaring List class object
-        // Declaring object of type List<DijkstraNode>
-        List<List<DijkstraNode> > adj
-                = new ArrayList<List<DijkstraNode> >();
-        // Initialize list for every DijkstraNode
-        for (int i = 0; i < N; i++) {
-            List<DijkstraNode> item = new ArrayList<DijkstraNode>();
-            adj.add(item);
-        }
-        // Inputs for the Tractor(dpq) graph
-        for (int i = 1; i<N+1; i++){
+        int x = Integer.parseInt(s[1]);
+        int y = Integer.parseInt(s[2]);
+        ArrayList<ArrayList<Integer>> points = new ArrayList<>(V);
+        int max_x = x;
+        int max_y = y;
+        for (int i = 0; i<V; i++){
             s = br.readLine().split(" ");
-            int x = Integer.parseInt(s[0]);
-            int y = Integer.parseInt(s[1]);
-            Location loc = new Location(x, y, i);
-            adj.get(0).add(new DijkstraNode(loc, 1));
+            x = Integer.parseInt(s[0]);
+            y = Integer.parseInt(s[1]);
+            ArrayList<Integer> t = new ArrayList<>();
+            t.add(x);
+            t.add(y);
+            points.add(t);
+            if (x>max_x) {
+                max_x = x;
+            }
+            if (y>max_y){
+                max_y = y;
+            }
         }
-        // Calculating the single source shortest path
-        Tractor dpq = new Tractor(N+1);
-        dpq.dijkstra(adj, source);
+        int[][] graph = new int[(max_x+1)*(max_y+1)][(max_x+1)*(max_y+1)];
+        ArrayList<Integer> processed_points = new ArrayList<>();
+        for (int i = 0; i<max_x; i++){
+            for (int j = 0; j<max_y; j++){
+                int v = twod_oned(x, y, max_x, max_y);
+                processed_points.add(v);
+            }
+        }
+        for (int i = 0; i<processed_points.size(); i++){
+            for (int j = 0; j<processed_points.size(); j++){
+                ArrayList<Integer> xy = oned_twodd(processed_points.get(i), max_x, max_y);
+                x = xy.get(0);
+                y = xy.get(1);
+                if (includes(points, x, y)){
+                    graph[processed_points.get(i)][processed_points.get(j)] = 0;
+                }else if (includes(points, x+1, y) || includes(points, x-1, y) || includes(points, x, y+1) || includes(points, x, y-1)) {
+                    graph[processed_points.get(i)][processed_points.get(j)] = 1;
+                } else{
+                    graph[processed_points.get(i)][processed_points.get(j)] = Integer.MAX_VALUE;
+                }
+            }
+        }
 
-        // Printing the shortest path to all the DijkstraNodes
-        // from the source DijkstraNode
-        System.out.println("The shorted path from DijkstraNode :");
-
-        for (int i = 0; i < dpq.dist.length; i++)
-            System.out.println(source + " to " + i + " is "
-                    + dpq.dist[i]);
-    }
-}
-
-// Class 2
-// Helper class implementing Comparator interface
-// Representing a DijkstraNode in the graph
-class DijkstraNode implements Comparator<DijkstraNode> {
-
-    // Member variables of this class
-    public Location DijkstraNode;
-    public int cost;
-
-    // Constructors of this class
-
-    // Constructor 1=
-    public DijkstraNode() {}
-
-    // Constructor 2
-    public DijkstraNode(Location DijkstraNode, int cost)
-    {
-
-        // This keyword refers to current instance itself
-        this.DijkstraNode = DijkstraNode;
-        this.cost = cost;
-    }
-
-    // Method 1
-    @Override public int compare(DijkstraNode DijkstraNode1, DijkstraNode DijkstraNode2)
-    {
-
-        if (DijkstraNode1.cost < DijkstraNode2.cost)
-            return -1;
-
-        if (DijkstraNode1.cost > DijkstraNode2.cost)
-            return 1;
-
-        return 0;
+        dijkstra(graph);
     }
 }
