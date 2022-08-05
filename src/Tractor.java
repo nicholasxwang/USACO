3999430d3959d540842fd3104a6c4a0e6afd410b
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-04 22:04:50 -0700

    fix :)

--- src/Tractor.java
+++ src/Tractor.java
@@ -1,28 +1,24 @@
 import java.io.*;
 import java.util.*;
 class Tractor {
-    static int V = 6;
-
-    public static int twod_oned(int x, int y, int size_x, int size_y){
-        return x*size_y + y;
-    }
-    public static ArrayList<Integer> oned_twodd(int id, int size_x, int size_y){
+    public static int twoD_to_oneD(int x, int y, int size_x, int size_y){ return x*size_y + y;}
+    public static ArrayList<Integer> oneD_to_twoD(int id, int size_x, int size_y){
         ArrayList<Integer> ar = new ArrayList<>();
         ar.add(id/size_y);
         ar.add(id%size_y);
         return ar;
     }
-
     public static boolean includes(ArrayList<ArrayList<Integer>> ar, int x, int y){
-        boolean bool = false;
-        for (int i = 0; i<ar.size(); i++){
-            if (ar.get(i).get(0) == x && ar.get(i).get(1) == y) {bool = true; break;}
-
-        }
-        return bool;
+        for (ArrayList<Integer> integers : ar) { if (integers.get(0) == x && integers.get(1) == y) return true; }
+        return false;
     }
 
-    public static int selectMinVertex(ArrayList<Integer> value, ArrayList<Boolean> processed){
+    public static boolean accessible(int x1, int y1, int x2, int y2){
+        if (x1 == x2 && Math.abs(y1-y2) == 1) return true;
+        if (y1 == y2 && Math.abs(x1-x2) == 1) return true;
+        return false;
+    }
+    public static int selectMinVertex(ArrayList<Integer> value, ArrayList<Boolean> processed, int V){
         int minimum = Integer.MAX_VALUE;
         int vertex = -1;
         for (int i = 0; i<V; i++){
@@ -34,6 +30,7 @@ class Tractor {
         return vertex;
     }
     public static void dijkstra(int graph[][], ArrayList<Integer> source){
+        int V = graph.length;
         int[] parent = new int[V];
         ArrayList<Integer> value = new ArrayList<>();
         ArrayList<Boolean> processed = new ArrayList<>();
@@ -44,7 +41,7 @@ class Tractor {
         parent[0] = -1;
         value.set(source.get(0), source.get(1));
         for (int i = 0; i<V; i++){
-            int U = selectMinVertex(value, processed);
+            int U = selectMinVertex(value, processed, V);
 
             for (int j = 0; j<V; j++){
                 if (graph[U][j] != 0 && !processed.get(j) && value.get(U) != Integer.MAX_VALUE &&
@@ -57,7 +54,7 @@ class Tractor {
 
             }
         }
-        for(int i=1;i<V;++i)
+        for(int i=0;i<V;i++)
             System.out.println("U->V: "+parent[i]+"->"+i+"  wt = "+graph[parent[i]][i]+"\n");
     }
     public static void main(String[] args) throws IOException {
@@ -66,7 +63,7 @@ class Tractor {
         int N = Integer.parseInt(s[0]);
         int x = Integer.parseInt(s[1]);
         int y = Integer.parseInt(s[2]);
-        ArrayList<ArrayList<Integer>> points = new ArrayList<>(V);
+        ArrayList<ArrayList<Integer>> points = new ArrayList<>(N);
         int max_x = x;
         int max_y = y;
         ArrayList<Integer> source = new ArrayList<>();
@@ -92,17 +89,17 @@ class Tractor {
         ArrayList<Integer> processed_points = new ArrayList<>();
         for (int i = 0; i<max_x; i++){
             for (int j = 0; j<max_y; j++){
-                int v = twod_oned(i, j, max_x, max_y);
+                int v = twoD_to_oneD(i, j, max_x, max_y);
                 processed_points.add(v);
             }
         }
         int[][] graph = new int[processed_points.size()][processed_points.size()];
         for (int i = 0; i<processed_points.size(); i++){
             for (int j = 0; j<processed_points.size(); j++){
-                ArrayList<Integer> xy = oned_twodd(processed_points.get(i), max_x, max_y);
+                ArrayList<Integer> xy = oneD_to_twoD(processed_points.get(i), max_x, max_y);
                 x = xy.get(0);
                 y = xy.get(1);
-                ArrayList<Integer> xy2 = oned_twodd(processed_points.get(j), max_x, max_y);
+                ArrayList<Integer> xy2 = oneD_to_twoD(processed_points.get(j), max_x, max_y);
                 int x2 = xy2.get(0);
                 int y2 = xy2.get(1);
                 int value = 9;
@@ -110,14 +107,8 @@ class Tractor {
                     value = 0;
                 }
                 else {
-                    boolean x_diff = x2 - x == 1 || x2 - x == -1;
-                    boolean y_diff = y2 - y == 1 || y2 - y == -1;
-                    if (x_diff && y_diff) {
-                        x_diff = false;
-                        y_diff = false;
-                    }
-                    if (x_diff || y_diff) {
-                        if (includes(points, x2, y2)) {
+                    if (accessible(x, y, x2, y2)){
+                        if (includes(points, x2, y2) && includes(points, x, y)) {
                             value = 1;
                         } else {
                             value = 0;
