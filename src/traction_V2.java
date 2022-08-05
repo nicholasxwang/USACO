31b91585ecf0b6957c1bb5905f61243559169ea5
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-05 13:15:56 -0700

    okº•.•º

--- src/traction_V2.java
+++ src/traction_V2.java
@@ -18,20 +18,26 @@ public class traction_V2 {
         return y1 == y2 && Math.abs(x1 - x2) == 1;
     }
 
-    public static int search_path(int[][] graph, int x, int y){
+    public static int search_path(int[][] graph, int x, int y, int max_x, int max_y, int[] visited){
         System.out.println("(" + x + "," + y + ")");
         if (x==0 && y==0) return 0;
-        if (x<0 || y<0 || x>=graph.length || y>=graph[0].length) return max_value;
-        int value1 = search_path(graph, x+1, y);
-        int value2 =  search_path(graph, x-1, y);
-        int value3 = search_path(graph, x, y+1);
-        int value4 = search_path(graph, x+1, y-1);
-        return Math.min(Math.min(value1, value2), Math.min(value3, value4)) + graph[x][y];
+        if (x<0 || y<0 || x>=max_x|| y>=max_y) return max_value;
+//        visited[twoD_to_oneD(x, y, max_y)] = 1;
+        if(visited[twoD_to_oneD(x,y,max_y)]!=-1) return visited[twoD_to_oneD(x,y,max_y)];
+        visited[twoD_to_oneD(x, y, max_y)] = 1;
+        int value1 = search_path(graph, x+1, y, max_x, max_y, visited);
+        int value2 =  search_path(graph, x-1, y, max_x, max_y, visited);
+        int value3 = search_path(graph, x, y+1, max_x, max_y, visited);
+        int value4 = search_path(graph, x+1, y-1, max_x, max_y, visited);
+        int var = Math.min(Math.min(value1, value2), Math.min(value3, value4));
+        //visited[twoD_to_oneD(x,y,max_y)] = var + graph[x][y];;
+        return var + graph[x][y];
 
     }
     static int max_value = 999999;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String[] s = br.readLine().split(" ");
         int N = Integer.parseInt(s[0]);
         int x = Integer.parseInt(s[1]);
@@ -95,7 +101,11 @@ public class traction_V2 {
                 graph[processed_points.get(i)][processed_points.get(j)] = value;
             }
         }
-        int result = search_path(graph, source.get(0), source.get(1));
+        int[] visited = new int[processed_points.size()];
+        for (int i = 0; i < processed_points.size(); i++) {
+            visited[i] = -1;
+        }
+        int result = search_path(graph, source.get(0), source.get(1), max_x, max_y, visited);
         System.out.println(result);
 
 
