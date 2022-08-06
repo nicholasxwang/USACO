1d01af4106dc04502143463d3d744594d10ac56c
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-06 14:31:55 -0700

    debugging

--- src/Mooyo.java
+++ src/Mooyo.java
@@ -1,28 +1,49 @@
 import java.io.*; import java.util.*;
 class Cell{  int x, y; public Cell(int x, int y){ this.x = x;this.y = y; }}
 public class Mooyo {
-    static boolean isValid(int[][] grid, int x, int y, int current) { return  (!(x < 0 || x >= grid.length || y < 0 || y >= grid[0].length  || grid[x][y] != 0 || grid[x][y] == current)); }
+    static boolean contains(ArrayList<Cell> visited, int x, int y) {
+        for (int i = 0; i < visited.size(); i++) {
+            if (visited.get(i).x == x && visited.get(i).y == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+    static void printgrid(int[][] grid) {
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+    static boolean isValid(int[][] grid, int x, int y, int current, ArrayList<Cell> visiting) { return  (!(x < 0 || x >= grid.length || y < 0 || y >= grid[0].length  || grid[x][y] == 0 || grid[x][y] != current) || contains(visiting, x, y)); }
     static void floodFill(int[][] grid, ArrayList<Cell> visiting,  ArrayList<ArrayList<Cell>> regions, int x, int y) {
         Vector<Cell> queue = new Vector<>();
         visiting.add(new Cell(x, y));
         queue.add(new Cell(x, y));
         int current = grid[x][y];
         ArrayList<Cell> currentRegion = new ArrayList<>();
+        currentRegion.add(new Cell(x, y));
         while (queue.size() > 0) {
             Cell currPixel = queue.get(queue.size() - 1);
             queue.remove(queue.size() - 1);
             int posX = currPixel.x;
             int posY = currPixel.y;
-            if (isValid(grid,  posX + 1, posY, current)) { visiting.add(new Cell(posX + 1, posY));  currentRegion.add(new Cell(posX + 1, posY)); queue.add(new Cell(posX + 1, posY));}
-            if (isValid(grid, posX - 1, posY, current)) { visiting.add(new Cell(posX - 1, posY)); currentRegion.add(new Cell(posX - 1, posY)); queue.add(new Cell(posX - 1, posY));}
-            if (isValid(grid, posX, posY + 1, current)) { visiting.add(new Cell(posX, posY+1));   currentRegion.add(new Cell(posX, posY+1));queue.add(new Cell(posX, posY + 1));}
-            if (isValid(grid, posX, posY - 1, current)) {visiting.add(new Cell(posX, posY-1)); currentRegion.add(new Cell(posX, posY-1));queue.add(new Cell(posX, posY - 1));}
+            if (isValid(grid,  posX + 1, posY, current, visiting) && !contains(visiting, posX+1, posY)) { visiting.add(new Cell(posX + 1, posY));  currentRegion.add(new Cell(posX + 1, posY)); queue.add(new Cell(posX + 1, posY));}
+            if (isValid(grid, posX - 1, posY, current, visiting) && !contains(visiting, posX-1, posY)) { visiting.add(new Cell(posX - 1, posY)); currentRegion.add(new Cell(posX - 1, posY)); queue.add(new Cell(posX - 1, posY));}
+            if (isValid(grid, posX, posY + 1, current, visiting) && !contains(visiting, posX, posY+1)) { visiting.add(new Cell(posX, posY+1));   currentRegion.add(new Cell(posX, posY+1));queue.add(new Cell(posX, posY + 1));}
+            if (isValid(grid, posX, posY - 1, current, visiting) && !contains(visiting, posX, posY-1)) {visiting.add(new Cell(posX, posY-1)); currentRegion.add(new Cell(posX, posY-1));queue.add(new Cell(posX, posY - 1));}
         }
         regions.add(currentRegion);
     }
     static void gravity(int[][] grid){
         for (int j = 0; j < grid[0].length; j++) {
-            int[] column = new int[grid[0].length];
+            int[] column = new int[grid.length];
             for (int i = 0; i < grid.length; i++) {
                 column[i] = grid[i][j];
             }
@@ -51,42 +72,43 @@ public class Mooyo {
         K = Integer.parseInt(st.nextToken());
         int[][] grid = new int[N][10];
         for (i = 0; i<N; i++){
-            st = new StringTokenizer(br.readLine(), "");
+            String[] s = br.readLine().split("");
             for (j = 0; j<10; j++){
-                grid[i][j] = Integer.parseInt(st.nextToken());
+                grid[i][j] = Integer.parseInt(s[j]);
             }
         }
-
+        printgrid(grid);
         while (true){
             ArrayList<Cell> visiting = new ArrayList<>();
             ArrayList<ArrayList<Cell>> regions = new ArrayList<>();
             for (i = 0; i<N; i++){
                 for (j = 0; j<10; j++){
-                    floodFill(grid, visiting, regions, i, j);
+                    if (grid[i][j] != 0 && !contains(visiting, i, j)) {
+                        floodFill(grid, visiting, regions, i, j);
+                    }
                 }
             }
             int region_count = 0;
 
             for (i = 0; i < regions.size(); i++) {
-                if (regions.get(i).size() < K) {
+                if (regions.get(i).size() >= K) {
                     region_count++;
-                }else{
+
                     for(j = 0; j < regions.get(i).size(); j++){
                         grid[regions.get(i).get(j).x][regions.get(i).get(j).y] = 0;
                     }
                 }
             }
-
+            //printgrid(grid);
             gravity(grid);
+            visiting.clear();
+            regions.clear();
+            //printgrid(grid);
             if (region_count == 0){
                 break;
             }
+
         }
-        for (i = 0; i<grid.length; i++){
-            for (j = 0; j<grid[0].length; j++){
-                pw.print(grid[i][j] + " ");
-            }
-            pw.println();
-        }
+        printgrid(grid);
     }
 }
