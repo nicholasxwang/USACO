cef88ec5de7ef62259b69089dfdea5bb9415b6d8
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-07 14:28:33 -0700

    more stuff

--- src/Lifeguards.java
+++ src/Lifeguards.java
@@ -6,23 +6,49 @@ public class Lifeguards {
         int N = Integer.parseInt(br.readLine());
         int[][] intervals = new int[N][2];
         int max_bound = 0;
-        for (int i = 0; i<N; i++) {
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-            intervals[i][0] = A;
-            intervals[i][1] = B;
-            if (B > max_bound) max_bound = B;
+            intervals[i][0] = A - 1;
+            intervals[i][1] = B - 1;
+            if (B - 1 > max_bound) max_bound = B - 1;
         }
-        int[] prefix = new int[max_bound+1];
-        for (int i = 0; i<N; i++) {
-            for (int j = intervals[i][0]; j<intervals[i][1]; j++) {
+        int[] prefix = new int[max_bound];
+        for (int i = 0; i < N; i++) {
+            for (int j = intervals[i][0]; j < intervals[i][1]; j++) {
                 prefix[j]++;
             }
         }
         System.out.println(Arrays.toString(prefix));
 
+        double[] averages = new double[N];
+        for (int i = 0; i < N; i++) {
+            // find average of interval
+            double average = 0;
+            for (int j = intervals[i][0]; j < intervals[i][1]-1; j++) {
+                average += prefix[j];
+            }
+            average /= (intervals[i][1] - intervals[i][0]);
+            averages[i] = average;
         }
+
+        int mininum_average = 0;
+        for (int i = 0; i < N; i++) {
+            if (averages[i] < averages[mininum_average]) {
+                mininum_average = i;
+            }
+        }
+
+
+        for (int i =0; i<prefix.length; i++){
+            if (intervals[i][0]-1 <= i && i < intervals[i][1]-1){
+                prefix[i]--;
+            }
+        }
+        System.out.println(Arrays.toString(prefix));
+    }
+
 
     }
 
