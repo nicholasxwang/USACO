963a4769e49e0eb165ac09fea1f315da31168eab
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-10 11:17:35 -0700

    stuff

--- src/HighCardWins.java
+++ src/HighCardWins.java
@@ -12,32 +12,26 @@ public class HighCardWins {
             int a = Integer.parseInt(st.nextToken());
             elsie.add(a);
         }
-        for (int i = 1; i<N*2+1; i++){
+        for (int i = N*2; i>=1; i--){
             if (!elsie.contains(i)){
                 bessie.add(i);
             }
         }
-        //sort both reverse
         Collections.sort(elsie, Collections.reverseOrder());
-        Collections.sort(bessie, Collections.reverseOrder());
+        //Collections.sort(bessie, Collections.reverseOrder());
         int answer = 0;
         while (!elsie.isEmpty() && !bessie.isEmpty()){
-            System.out.println(elsie + "\n\n" + bessie+"\n\n\n");
+            //System.out.println(elsie + "\n\n" + bessie+"\n\n\n");
             if (bessie.get(0) > elsie.get(0)){
                 answer++;
                 bessie.remove(0);
                 elsie.remove(0);
             }
             else{
-
-//                bessie.remove(0);
                 elsie.remove(0);
-//                elsie.remove(elsie.size()-1);
-//                bessie.remove(elsie.size()-1);
                 bessie.remove(bessie.size()-1);
             }
         }
-        //System.out.println(answer);
         PrintWriter pw = new PrintWriter(new FileWriter("highcard.out"));
         pw.println(answer);
         pw.close();
