98a24b1be46afe6815e75ea3eba646e80d74b424
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-11 09:32:53 -0700

    stuff

--- src/CircularBarn.java
+++ src/CircularBarn.java
@@ -1,5 +1,11 @@
 import java.io.*; import java.util.*;
 public class CircularBarn {
+    public static boolean check_all_ones(int[] a){
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=1) return false;
+        }
+        return true;
+    }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new FileReader("cbarn.in"));
         PrintWriter pw = new PrintWriter(new FileWriter("cbarn.out"));
@@ -10,24 +16,32 @@ public class CircularBarn {
         }
         ArrayList<Integer> zeroes = new ArrayList<Integer>();
         int distance = 0;
-        for (int i = 0; i<N; i++){
-            System.out.println(Arrays.toString(barns));
-            if (barns[i] == 0){
-                zeroes.add(i);
-            }else{
-                if (zeroes.size() == 0){
-                    continue;
-                }
-                //move it as front as possible
-                int most_front = zeroes.get(0);
-                barns[most_front]--;
-                distance = (i - most_front) * (i - most_front);
-                zeroes.remove(0);
+        while (!check_all_ones(barns)){
+            System.out.println("again");
+            for (int i = 0; i<N; i++){
                 if (barns[i] == 0){
                     zeroes.add(i);
-                }
+                }else{
+                    if (zeroes.size() == 0){
+                        continue;
+                    }
+                    //move it as front as possible
+                    int most_front = zeroes.get(0);
+                    barns[i] = barns[i] - 1;
+                    barns[most_front] = barns[most_front]+1;
+                    distance += (i - most_front) * (i - most_front);
+                    zeroes.remove(0);
+                    System.out.println(Arrays.toString(barns)+" "+distance);
+                    System.out.println(zeroes.toString());
 
+                    if (barns[i] == 0){
+                        zeroes.add(i);
+                    }
+
+                }
             }
+            System.out.println("again");
+            System.out.println(Arrays.toString(barns)+" "+distance);
         }
         System.out.println(distance);
     }
