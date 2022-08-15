ee5be5f40817027b556546f86fd5ee4f4901f3b4
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-15 11:35:37 -0700

    some stuff
    Former-commit-id: 5fe55595c7b2693a7acc5432f4988d84341a2baf

--- src/MountainView.java
+++ src/MountainView.java
@@ -23,16 +23,34 @@ class Mountain{
     }
 
 
-
-
 }
 public class MountainView {
-//    public static check_mountain9
+//    public static check_mountain9]
+
+    static float sign (MountainPoint p1, MountainPoint p2, MountainPoint p3) {
+        return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+    }
+
+    static boolean PointInTriangle (MountainPoint pt, MountainPoint v1, MountainPoint v2, MountainPoint v3)
+    {
+        float d1, d2, d3;
+        boolean has_neg, has_pos;
+
+        d1 = sign(pt, v1, v2);
+        d2 = sign(pt, v2, v3);
+        d3 = sign(pt, v3, v1);
+
+        has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+        has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+        return !(has_neg && has_pos);
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        Queue<Mountain> mountains = new LinkedList<>();
+        ArrayList<Mountain> mountains = new ArrayList<>();
         for (int i = 0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
@@ -40,5 +58,34 @@ public class MountainView {
             Mountain mtn = new Mountain(x, y);
             mountains.add(mtn);
         }
+        boolean stable = false;
+        while (!stable){
+            boolean small_stable = true;
+            for (int  i =0; i<mountains.size(); i++){
+               for (int j = 0; j<mountains.size(); j++){
+                   if (i==j) continue;
+                  if (!PointInTriangle(mountains.get(i).peak, mountains.get(j).peak, mountains.get(j).left, mountains.get(j).right) )
+                    small_stable = false;
+                    mountains.remove(i);
+                    break;
+
+
+
+               }
+
+            }
+            if (small_stable) stable = true;
+            if (stable){
+                break;
+            }
+
+
+        }
+        System.out.println(mountains.size());
+
+
+
+
+
     }
 }
--- src/SecretCowCode.java
+++ src/SecretCowCode.java
@@ -4,26 +4,25 @@ public class SecretCowCode {
        BufferedReader br = new BufferedReader(new FileReader("cowcode.in"));
        PrintWriter pw = new PrintWriter("cowcode.out");
        StringTokenizer st = new StringTokenizer(br.readLine());
-       char[] original    = st.nextToken().toCharArray();
-       long N = Integer.parseInt(st.nextToken());
-
+       char[] original = st.nextToken().toCharArray();
+       long N = Long.parseLong(st.nextToken());
        char[] array = new char[(int) N];
        int index = original.length;
-       for (int i = 0; i<original.length; i++){
-           array[i] = original[i];
-       }
-        boolean break_twice = false;
+       for (int i = 0; i<original.length; i++) array[i] = original[i];
+       boolean break_twice = false;
        while (true){
            if (index == N || break_twice){
                pw.println(array[(int) (N-1)]);
                break;
            }
            int stop = index-1;
+           //DONT make a copy of the array!
+
+
            array[index] = array[index-1];
            index++;
            for (int i = 0; i<stop; i++){
                if (index == N){
-                   //System.out.println(array[N-1]);
                    break_twice = true;
                    break;
                }
@@ -31,6 +30,7 @@ public class SecretCowCode {
                index++;
 
            }
+
 
        }
        pw.close();
