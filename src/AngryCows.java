5bdc5286ebcfb8d3377eaaffb08679898127c682
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-16 09:01:15 -0700

    stuff
    Former-commit-id: 88b6b1652c8e943304b5013c1df9559625399a20

--- src/AngryCows.java
+++ src/AngryCows.java
@@ -15,16 +15,34 @@ public class AngryCows {
         for (int i = 0; i<bales.length; i++){
             sorted_bales.add(bales[i]);
         }
-        int V = 5;
-        int A = 1;
-        int starting = sorted_bales.get(0);
-        for (int i = 0; i<N; i++){
-            if (sorted_bales.get(i) > starting + V*2){
-                A++;
-                starting = sorted_bales.get(i);
-            }
+        int low = 1;
+        int high = N;
+        int mid = 0;
+        int smallest = N;
+        int[] answers = new int[N];
+        for (int i = 0; i<answers.length; i++){
+            answers[i] = -1;
         }
-        System.out.println(A);
+        while (low!=high){
+            int V = (low + high)/2;
+            int R = 1;
+            int starting = sorted_bales.get(0);
+            for (int i = 0; i<N; i++){
+                if (sorted_bales.get(i) > starting + V*2){
+                    R++;
+                    starting = sorted_bales.get(i);
+                }
+            }
+            System.out.println(V+" -> "+R);
+
+            if (R > smallest) // x is on the right side
+                low = mid + 1;
+
+            else                  // x is on the left side
+                high = mid - 1;
+        }
+
+
 
 
 
