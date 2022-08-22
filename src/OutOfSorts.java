8842e6e59c9f65e45838e442a343840e53c607c8
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-21 20:24:23 -0700

    stuff

--- src/OutOfSorts.java
+++ src/OutOfSorts.java
@@ -1,31 +1,37 @@
 import java.io.*; import java.util.*;
 
+class Entry{
+    int value;
+    int index;
+
+    public Entry(int value, int index){
+        this.value = value;
+        this.index = index;
+    }
+}
 public class OutOfSorts {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("sort.in"));
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
+        Entry[] A = new Entry[N];
         for (int i = 0; i<N; i++){
-            A[i] = Integer.parseInt(br.readLine());
+            A[i] = new Entry(Integer.parseInt(br.readLine()), i);
         }
-        int[] sorted = new int[N];
-        for (int i = 0; i<N; i++){
-            sorted[i] = A[i];
-        }
-        Arrays.sort(sorted);
-        for (int i = 0; i<N; i++){
-            //set each element of the array to location in sorted
-            for (int j = 0; j<N; j++){
-                if (A[i] == sorted[j]){
-                    A[i] = j;
-                    break;
+
+        Arrays.sort(A, new Comparator<Entry>(){
+            public int compare(Entry e1, Entry e2){
+                if (e1.value == e2.value){
+                    return e1.index - e2.index;
                 }
+                return e1.value - e2.value;
             }
-        }
+        });
+
         int max = 0;
         for (int i = 0; i<N; i++){
-            if (i - A[i] > max)
-                max = i - A[i];
+            if (A[i].value - A[i].index > max){
+                max = A[i].value - A[i].index;
+            }
         }
 
         PrintWriter pw = new PrintWriter("sort.out");
