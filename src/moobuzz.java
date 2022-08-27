b7a81dc7efa91dc46a5cc9bc22938090cf6b8b9b
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-08-27 08:55:44 -0700

    stuff

--- src/moobuzz.java
+++ src/moobuzz.java
@@ -2,18 +2,23 @@ import java.io.*;
 
 public class moobuzz {
     public static void main(String[] args) throws IOException{
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader br = new BufferedReader(new FileReader("moobuzz.in"));
         int count = 0;
         int seqcount = 0;
         int N = Integer.parseInt(br.readLine());
+
+        while (N > 8){
+            N = N - 8;
+//            seqcount += 15;
+            count += 15;
+            seqcount += 8;
+        }
         while (seqcount != N){
             count++;
             if (!(count % 3 == 0) & !(count % 5 == 0)){
                 seqcount++;
             }
         }
-//        System.out.println(count);
         PrintWriter pw = new PrintWriter("moobuzz.out");
         pw.println(count);
         pw.close();
