8e615c4d824c20b86b316491df163571cfb79f28
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-09-10 17:56:44 -0700

    stuff

--- src/NoTimeToPaint.java
+++ src/NoTimeToPaint.java
@@ -17,34 +17,32 @@ public class NoTimeToPaint {
 
             Stack<Character> stack = new Stack<>();
             for (int p = 0; p<small-1; p++){
-                if (!stack.contains(s[p])){
-                    stack.add(s[p]);
-                    strokes++;
-                }
                 for (int j = 0; j<stack.size(); j++ ){
                     if (stack.get(j) > s[p]){
                         stack.remove(stack.get(j));
                     }
-//                    if (stack.get(j) > s[p]){
-//                        strokes++;
-//                    }
+                }
+                if (!stack.contains(s[p])){
+                    stack.add(s[p]);
+                    strokes++;
                 }
             }
 
 
             //range 2: (big + 1) -> N-1
 
-            stack = new Stack<>();
+
+            stack.clear();
             for (int p = big; p<N; p++){
-                if (!stack.contains(s[p])){
-                    stack.add(s[p]);
-                    strokes++;
-                }
                 for (int j = 0; j<stack.size(); j++ ){
                     if (stack.get(j) > s[p]){
                         stack.remove(stack.get(j));
                     }
 
+                }
+                if (!stack.contains(s[p])){
+                    stack.add(s[p]);
+                    strokes++;
                 }
             }
 
