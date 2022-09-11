3d8fcc961ae5995459d1f6b962fad6c1486c3c15
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-09-10 18:08:34 -0700

    more stuff

--- src/NoTimeToPaint.java
+++ src/NoTimeToPaint.java
@@ -14,38 +14,40 @@ public class NoTimeToPaint {
             int big = Integer.parseInt(st.nextToken());
             int strokes = 0;
             //range 1: 1 -> (small-1)
-
             Stack<Character> stack = new Stack<>();
             for (int p = 0; p<small-1; p++){
+                ArrayList<Character> to_be_removed = new ArrayList<>();
                 for (int j = 0; j<stack.size(); j++ ){
                     if (stack.get(j) > s[p]){
-                        stack.remove(stack.get(j));
+                        to_be_removed.add(stack.get(j));
                     }
+                }
+                for(int j=0; j<to_be_removed.size(); j++){
+                    stack.remove(to_be_removed.get(j));
                 }
                 if (!stack.contains(s[p])){
                     stack.add(s[p]);
                     strokes++;
                 }
             }
-
-
             //range 2: (big + 1) -> N-1
-
-
             stack.clear();
             for (int p = big; p<N; p++){
+                ArrayList<Character> to_be_removed = new ArrayList<>();
                 for (int j = 0; j<stack.size(); j++ ){
                     if (stack.get(j) > s[p]){
-                        stack.remove(stack.get(j));
+                       to_be_removed.add(stack.get(j));
                     }
 
+                }
+                for(int j=0; j<to_be_removed.size(); j++){
+                    stack.remove(to_be_removed.get(j));
                 }
                 if (!stack.contains(s[p])){
                     stack.add(s[p]);
                     strokes++;
                 }
             }
-
             System.out.println(strokes);
         }
     }
