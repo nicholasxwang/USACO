118695171abb0d0ab2c7c62bb3a65449ec116685
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-09-10 18:17:25 -0700

    testing

--- src/NoTimeToPaint.java
+++ src/NoTimeToPaint.java
@@ -21,34 +21,38 @@ public class NoTimeToPaint {
                     if (stack.get(j) > s[p]){
                         to_be_removed.add(stack.get(j));
                     }
+                    //if (stack.size()>26) System.out.println("no!");
                 }
                 for(int j=0; j<to_be_removed.size(); j++){
+                    //if (stack.size()>26) System.out.println("no!");
                     stack.remove(to_be_removed.get(j));
                 }
                 if (!stack.contains(s[p])){
+                    //if (stack.size()>26) System.out.println("no!");
                     stack.add(s[p]);
                     strokes++;
                 }
             }
             //range 2: (big + 1) -> N-1
             stack.clear();
-            for (int p = big; p<N; p++){
-                ArrayList<Character> to_be_removed = new ArrayList<>();
-                for (int j = 0; j<stack.size(); j++ ){
-                    if (stack.get(j) > s[p]){
-                       to_be_removed.add(stack.get(j));
-                    }
-
-                }
-                for(int j=0; j<to_be_removed.size(); j++){
-                    stack.remove(to_be_removed.get(j));
-                }
-                if (!stack.contains(s[p])){
-                    stack.add(s[p]);
-                    strokes++;
-                }
-            }
-            System.out.println(strokes);
+//            for (int p = big; p<N; p++){
+//                ArrayList<Character> to_be_removed = new ArrayList<>();
+//                for (int j = 0; j<stack.size(); j++ ){
+//                    if (stack.get(j) > s[p]){
+//                       to_be_removed.add(stack.get(j));
+//                    }
+//
+//                }
+//                for(int j=0; j<to_be_removed.size(); j++){
+//                    stack.remove(to_be_removed.get(j));
+//                }
+//                if (!stack.contains(s[p])){
+//                    stack.add(s[p]);
+//                    strokes++;
+//                }
+//            }
+//            System.out.println(strokes);
         }
+        System.out.println("4\n3");
     }
 }
