d33c59cf841a184412f11ab1c808855e0e93c52e
Nicholas Wang <nicholas.x.wang@gmail.com>
2022-09-17 09:39:04 -0700

    fixed: bug (some testcases have very big negative integers so you need to mod the numbers and make negative into posittive)

--- src/triangles.java
+++ src/triangles.java
@@ -11,12 +11,12 @@ class TriangularPoint{
 }
 public class triangles {
     public static Integer area(TriangularPoint p1, TriangularPoint p2, TriangularPoint p3) {
-        int one_x = (int) p1.x;
-        int two_x = (int) p2.x;
-        int three_x = (int) p3.x;
-        int one_y = (int) p1.y;
-        int two_y = (int) p2.y;
-        int three_y = (int) p3.y;
+        int one_x = p1.x;
+        int two_x = p2.x;
+        int three_x = p3.x;
+        int one_y = p1.y;
+        int two_y = p2.y;
+        int three_y = p3.y;
         int sum1 = 0;
         sum1 += (one_x * two_y);
         sum1 += (two_x * three_y);
@@ -37,7 +37,7 @@ public class triangles {
 //        0 1
 //        1 0
 //        1 2
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new FileReader("triangles.in"));
         int N = Integer.parseInt(br.readLine());
 //        HashMap<Integer, ArrayList<TriangularPoint>> x_values = new HashMap<>();
 //        HashMap<Integer, ArrayList<TriangularPoint>> y_values = new HashMap<>();
@@ -47,13 +47,13 @@ public class triangles {
         ArrayList<TriangularPoint> arr = new ArrayList<>();
         for (int i = 0; i<N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken())+20000;
+            int y = Integer.parseInt(st.nextToken())+20000;
             TriangularPoint point = new TriangularPoint(x, y);
-            if (!x_values.keySet().contains(x)){
+            if (!x_values.containsKey(x)){
                 x_values.put(x, new ArrayList<>());
             }
-            if (!y_values.keySet().contains(y)){
+            if (!y_values.containsKey(y)){
                 y_values.put(y, new ArrayList<>());
             }
 //            x_values.get(x).add(point);
@@ -68,21 +68,25 @@ public class triangles {
             TriangularPoint point = arr.get(i);
             ArrayList<Integer> x = x_values.get(point.x);
             ArrayList<Integer> y = y_values.get(point.y);
-            for (int a = 0; a<x.size() ;a++){
+            for (Integer a : x) {
                 //if (x.get(a) <= i) continue;
-                if (x.get(a) == i) continue;
-                for (int b = 0; b<y.size(); b++){
-                   // if (y.get(b) <= i) continue;
-                    if (y.get(b) == i || y.get(b) == x.get(a)) continue;
-                    int area  = area(arr.get(i), arr.get(x.get(a)), arr.get(y.get(b)));
+                if (a == i) continue;
+                for (Integer b : y) {
+                    // if (y.get(b) <= i) continue;
+                    if (b == i || Objects.equals(b, a)) continue;
+                    int area = area(arr.get(i), arr.get(a), arr.get(b));
 //                    System.out.println("Triangle: "+i+" "+x.get(a)+" "+y.get(b));
-                    sum+=area;
+                    area = area % (10*10*10*10*10*10*10*10*10+7);
+                    sum = sum % (10*10*10*10*10*10*10*10*10+7);
+                    sum += area;
 
                 }
             }
 
         }
-        System.out.println(sum);
+       PrintWriter pw = new PrintWriter("triangles.out");
+        pw.println(sum);
+        pw.close();
 
 
 
