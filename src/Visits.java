eeb63c61dc598c77c91b04b8ad56478f4a9ad5c0
nicholasxwang <80929436+nicholasxwang@users.noreply.github.com>
2022-09-25 12:04:25 -0700

    first revision

--- src/Visits.java
+++ src/Visits.java
@@ -2,60 +2,67 @@ import java.io.*;
 import java.util.*;
 
 
-class NetworkEdge{
-    int c1;  //cow visiting
-    int c2; //cow being visted
-    int w; //number of moos
-    public NetworkEdge(int c1, int c2, int w){
-        this.c1 = c1;
-        this.c2 = c2;
-        this.w = w;
-    }
-}
-
-class NetworkNode{
-    int vistors; //number of visitors
-    int id;
-    public NetworkNode(int id){
-        this.vistors = 0;
-        this.id = id;
-    }
-
-}
 public class Visits {
+    public static int selectMinVertex(int[] value, boolean[] setMST, int V){
+        int min = 99999999;
+        int vertex = 0;
+        for (int i = 0; i<V; i++){
+            if (setMST[i] == false && value[i]<min){
+                vertex = i;
+                min = value[i];
+            }
+        }
+        return vertex;
+    }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        ArrayList<NetworkEdge> edges = new ArrayList<>();
-        ArrayList<NetworkNode> nodes = new ArrayList<>();
-        for (int i = 0; i<N; i++){
-            nodes.add(new NetworkNode(i));
-        }
-        for (int i = 0; i<N; i++){
+        int[][] adj = new int[N][N];
+        int[] dist = new int[N];
+        boolean[] mstSet = new boolean[N];
+        int[] parent = new int[N];
+        for (int i = 0; i<N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int cow = Integer.parseInt(st.nextToken())-1;
-            int weight = Integer.parseInt(st.nextToken());
-            edges.add(new NetworkEdge(i, cow, weight));
-            nodes.get(cow).vistors = nodes.get(cow).vistors+1;
+            int j = Integer.parseInt(st.nextToken())-1;
+            int w = Integer.parseInt(st.nextToken());
+            adj[i][j] = w;
+            adj[j][i] = w;
         }
-
-        Collections.sort(edges, new Comparator<NetworkEdge>() {
-            public int compare(NetworkEdge o1, NetworkEdge o2) {
-                if (o1.w != o2.w) return o2.w - o1.w;
-                return 0;
+        for (int i = 0; i<N; i++) {
+            if (i==0) {
+                dist[0] = 0;
+            } else{
+                dist[i] = 999999999;
             }
-        });
-        ArrayList<Integer> blacklisted = new ArrayList<>();
-        int MAX = 0;
-        for (int i = 0; i<edges.size(); i++){
-            NetworkEdge current = edges.get(i);
-            if (blacklisted.contains(nodes.get(current.c1).id)) continue;
-            blacklisted.add(current.c2);
-            //System.out.println(blacklisted);
-            MAX+=current.w;
+            mstSet[i] = false;
+            parent[i] = -1;
         }
-        System.out.println(MAX);
 
+
+
+        for (int i = 0; i<N-1; i++){
+            int U = selectMinVertex(dist, mstSet, N);
+            mstSet[U] = true;
+
+            for (int j = 0; j<N; j++){
+                if (adj[U][j]!=0 && mstSet[j] == false && adj[U][j] < dist[j]){
+                    dist[j] = adj[U][j];
+                    parent[j] = U;
+                }
+            }
+        }
+        //Print MST
+        int ans = 0;
+        for(int i=1;i<N;++i) {
+            //System.out.println("U->V: " + parent[i] + "->" + i + "  wt = " + adj[parent[i]][i] + "\n");
+            if (parent[i] == -1){
+                ans = -1;
+                break;
+            }else{
+                ans +=adj[parent[i]][i];
+            }
+
+        }
 
 
     }
