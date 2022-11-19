0b9eeb7fa2c95c83c9d2c7835cb71fab2142c580
nicholasxwang <80929436+nicholasxwang@users.noreply.github.com>
2022-11-19 15:36:28 -0800

    fixed some time complexity issues

--- src/ConvolutedIntervals.java
+++ src/ConvolutedIntervals.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*; import java.util.*;
 
 public class ConvolutedIntervals {
     public static void main(String[] args) throws IOException {
@@ -15,25 +12,31 @@ public class ConvolutedIntervals {
             intervals[i][0] = Integer.parseInt(st.nextToken());
             intervals[i][1] = Integer.parseInt(st.nextToken());
         }
-        int[][] xsums = new int[N][N];
-        int[][] ysums = new int[N][N];
+        int[][] x_sums = new int[N][N];
+        int[][] y_sums = new int[N][N];
         for (int i = 0; i<N; i++){
             for (int j = 0; j<N; j++){
-                xsums[i][j] = intervals[i][0] + intervals[j][0];
-                ysums[i][j] = intervals[i][1] + intervals[j][1];
+                x_sums[i][j] = intervals[i][0] + intervals[j][0];
+                y_sums[i][j] = intervals[i][1] + intervals[j][1];
             }
         }
-        for (int k = 0; k<= 2*M; k++){
-            int ans = 0;
-            for (int i = 0; i<N; i++){
-                for (int j = 0; j<N; j++){
-                    if (xsums[i][j] <= k && ysums[i][j] >= k){
-                        ans++;
-                    }
+        int[] answers = new int[M*2+1];
+        for (int i = 0; i<N; i++){
+            for (int j = 0; j<N; j++){
+                int bottom = x_sums[i][j];
+                int top = y_sums[i][j];
+                if (bottom > top) continue;
+                for (int k = bottom; k<=top; k++){
+                    answers[k]++;
                 }
             }
-            System.out.println(ans);
         }
+        String answer = "";
+        for (int k = 0; k<= 2*M; k++){
+            answer+=answers[k];
+            if (k != 2*M) answer+="\n";
+        }
+        System.out.println(answer);
 
     }
 }
