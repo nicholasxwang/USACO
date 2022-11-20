d89489b9ee5bc96cb6b5a3845079e5bd93189db5
nicholasxwang <80929436+nicholasxwang@users.noreply.github.com>
2022-11-20 10:20:04 -0800

    more stuff

--- src/ConvolutedIntervals.java
+++ src/ConvolutedIntervals.java
@@ -7,30 +7,41 @@ public class ConvolutedIntervals {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[][] intervals = new int[N][2];
+        int[][] x_sums = new int[N][N];
+        int[][] y_sums = new int[N][N];
+        int[] prefixx = new int[M*2+1];
+        int[] prefixy = new int[M*2+1];
+        int[] answers = new int[M*2+1];
         for (int i = 0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             intervals[i][0] = Integer.parseInt(st.nextToken());
             intervals[i][1] = Integer.parseInt(st.nextToken());
         }
-        int[][] x_sums = new int[N][N];
-        int[][] y_sums = new int[N][N];
         for (int i = 0; i<N; i++){
             for (int j = 0; j<N; j++){
                 x_sums[i][j] = intervals[i][0] + intervals[j][0];
                 y_sums[i][j] = intervals[i][1] + intervals[j][1];
-            }
-        }
-        int[] answers = new int[M*2+1];
-        for (int i = 0; i<N; i++){
-            for (int j = 0; j<N; j++){
                 int bottom = x_sums[i][j];
                 int top = y_sums[i][j];
-                if (bottom > top) continue;
-                for (int k = bottom; k<=top; k++){
-                    answers[k]++;
-                }
+                prefixx[bottom]++;
+                prefixy[top]++;
             }
         }
+        //run prefix sums
+        // run y prefix sums from 0 to end
+        for (int i = 1; i<M*2+1; i++){
+            prefixx[i] += prefixx[i-1];
+        }
+        // run x prefix sums from end to 0
+        for (int i = M*2-1; i>=0; i--){
+            prefixy[i] += prefixy[i+1];
+        }
+
+        for (int i = 0; i<answers.length; i++){
+            // take the smaller of the two
+            answers[i] = Math.min(prefixx[i], prefixy[i]);
+        }
+
         String answer = "";
         for (int k = 0; k<= 2*M; k++){
             answer+=answers[k];
