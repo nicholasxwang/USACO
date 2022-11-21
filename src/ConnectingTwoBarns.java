71351662ef0399f763d5e3d97249e4773ed1e636
nicholasxwang <80929436+nicholasxwang@users.noreply.github.com>
2022-11-21 08:15:40 -0800

    fixed some small bugs

--- src/ConnectingTwoBarns.java
+++ src/ConnectingTwoBarns.java
@@ -1,29 +1,29 @@
 import java.io.*; import java.util.*;
 
-class ConnectingNodes{
-    int id;
-    ArrayList<ConnectingEdge> connections;
-    public ConnectingNodes(int id){
-        this.id = id;
-        connections = new ArrayList<ConnectingEdge>();
-    }
-}
-class ConnectingEdge{
-   int weight;
-   ConnectingNodes node1;
-    ConnectingNodes node2;
-    public ConnectingEdge(int weight, ConnectingNodes node1, ConnectingNodes node2){
-        this.weight = weight;
-        this.node1 = node1;
-        this.node2 = node2;
-    }
-}
+//class ConnectingNodes{
+//    int id;
+//    ArrayList<ConnectingEdge> connections;
+//    public ConnectingNodes(int id){
+//        this.id = id;
+//        connections = new ArrayList<ConnectingEdge>();
+//    }
+//}
+//class ConnectingEdge{
+//   int weight;
+//   ConnectingNodes node1;
+//    ConnectingNodes node2;
+//    public ConnectingEdge(int weight, ConnectingNodes node1, ConnectingNodes node2){
+//        this.weight = weight;
+//        this.node1 = node1;
+//        this.node2 = node2;
+//    }
+//}
 public class ConnectingTwoBarns {
     public static int selectMinVertex(ArrayList<Integer> value, ArrayList<Boolean> processed, int V){
         int minimum = Integer.MAX_VALUE;
         int vertex = -1;
         for (int i = 0; i<V; i++){
-            if (!processed.get(i) && value.get(i)<minimum){
+            if (!processed.get(i) && value.get(i)<minimum){ 
                 minimum = value.get(i);
                 vertex = i;
             }
@@ -31,7 +31,7 @@ public class ConnectingTwoBarns {
         return vertex;
     }
 
-    public static void dijkstra(int graph[][], int V)
+    public static int dijkstra(int graph[][], int V)
     {
 
         int[] parent = new int[V];	//Stores Shortest Path Structure
@@ -47,7 +47,7 @@ public class ConnectingTwoBarns {
 
         //Assuming start point as Node-0
         parent[0] = -1;	//Start node has no parent
-        value.add(0);	//start node has value=0 to get picked 1st
+        value.set(0, 0);	//start node has value=0 to get picked 1st
 
 
         //Include (V-1) edges to cover all V-vertices
@@ -65,7 +65,7 @@ public class ConnectingTwoBarns {
 			      2.Vertex j is not included in shortest path graph
 			      3.Edge weight is smaller than current edge weight
 			*/
-                if(graph[U][j]!=0 && processed.get(j) ==false && value.get(U) != Integer.MAX_VALUE
+                if(graph[U][j]!=-1 && processed.get(j) ==false && value.get(U) != Integer.MAX_VALUE
                         && (value.get(U) +graph[U][j] < value.get(j)))
                 {
                     value.set(j, value.get(U) + graph[U][j]);
@@ -74,8 +74,10 @@ public class ConnectingTwoBarns {
             }
         }
         //Print Shortest Path Graph
-        for(int i=1;i<V;++i)
-            System.out.println("U->V: "+parent[i]+"->"+i+"  wt = "+graph[parent[i]][i]+"\n");
+//        for(int i=1;i<V;++i)
+//            System.out.println("U->V: "+parent[i]+"->"+i+"  wt = "+graph[parent[i]][i]+"\n");
+        //return path length
+        return value.get(V-1);
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -84,10 +86,10 @@ public class ConnectingTwoBarns {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
-            ConnectingNodes[] nodes = new ConnectingNodes[N];
-            for (int j = 0; j<N; j++){
-                nodes[j] = new ConnectingNodes(j);
-            }
+//            ConnectingNodes[] nodes = new ConnectingNodes[N];
+//            for (int j = 0; j<N; j++){
+//                nodes[j] = new ConnectingNodes(j);
+//            }
             int[][] edgeMatrix = new int[N][N];
             for (int j = 0; j<N; j++){
                 for (int k = 0; k<N; k++){
@@ -99,8 +101,10 @@ public class ConnectingTwoBarns {
                 int a = Integer.parseInt(st.nextToken())-1;
                 int b = Integer.parseInt(st.nextToken())-1;
                 edgeMatrix[a][b] = 0;
+                edgeMatrix[b][a] = 0;
             }
-            dijkstra(edgeMatrix, N);
+            int var = dijkstra(edgeMatrix, N);
+            System.out.println(var);
         }
     }
 }
